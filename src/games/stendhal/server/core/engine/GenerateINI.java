ed4ace4b7a9956be484ce5493f1351a54b13b6c1
ZEKKEQ <zekkeq@o2.pl>
2019-05-25 01:10:10 +0200

    Zmiana czasu serwera z UTC na Europe/Warsaw

--- src/games/stendhal/server/core/engine/GenerateINI.java
+++ src/games/stendhal/server/core/engine/GenerateINI.java
@@ -249,7 +249,7 @@ public class GenerateINI {
 		out.println();
 		out.println("# Database information. Edit to match your configuration.");
 		if (databaseSystem.equals("mysql")) {
-			out.println("jdbc_url=jdbc:mysql://" + databaseHost + "/" + databaseName + "?useUnicode=yes&characterEncoding=UTF-8&serverTimezone=UTC");
+			out.println("jdbc_url=jdbc:mysql://" + databaseHost + "/" + databaseName + "?useUnicode=yes&characterEncoding=UTF-8&serverTimezone=Europe/Warsaw");
 			out.println("jdbc_class=com.mysql.cj.jdbc.Driver");
 			out.println("jdbc_user=" + databaseUsername);
 			out.println("jdbc_pwd=" + databasePassword);
