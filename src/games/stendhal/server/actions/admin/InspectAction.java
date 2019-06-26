4b8237b5d3e0bae1cf3b01b9c787270c78460fda
ZEKKEQ <zekkeq@o2.pl>
2019-06-26 13:05:26 +0200

    Update InspectAction.java

--- src/games/stendhal/server/actions/admin/InspectAction.java
+++ src/games/stendhal/server/actions/admin/InspectAction.java
@@ -74,27 +74,27 @@ public class InspectAction extends AdministrationAction {
 
 			// st.append(target.toString());
 			// st.append("\n===========================\n");
-			st.append("\nZnajduje się na mapie: &" + inspected.getZone().getName() + " (" + inspected.getX() + ", " + inspected.getY() + ")");
-			st.append("\nID:          " + action.get("target"));
-			st.append("\nPłeć:       " + inspected.getGender());
-			st.append("\nATK:       " + inspected.getAtk() + "("
+			st.append("\nID: " + action.get("target") + " w &" + inspected.getZone().getName() + " (" + inspected.getX() + ", " + inspected.getY() + ")");
+			st.append("\nPłeć:  " + inspected.getGender());
+			st.append("\nATK:    " + inspected.getAtk() + "("
 					+ inspected.getAtkXP() + ")");
-			st.append("\nDEF:       " + inspected.getDef() + "("
+			st.append("\nOBR:    " + inspected.getDef() + "("
 					+ inspected.getDefXP() + ")");
-			st.append("\nRATK:     " + inspected.getRatk() + "("
+			st.append("\nSTR:    " + inspected.getRatk() + "("
 					+ inspected.getRatkXP() + ")");
-			st.append("\nPŻ:          " + inspected.getHP() + " / "
+			st.append("\nPZ:     " + inspected.getHP() + " / "
 					+ inspected.getBaseHP());
-			st.append("\nXP:          " + inspected.getXP());
-			st.append("\nPoziom:       " + inspected.getLevel());
-			st.append("\nKarma:       " + inspected.getKarma());
-			//st.append("\nMana:	" + inspected.getMana() + " / " + inspected.getBaseMana());
+			st.append("\nXP:     " + inspected.getXP());
+			st.append("\nPoziom:  " + inspected.getLevel());
+			st.append("\nKarma:  " + inspected.getKarma());
+			//st.append("\nMana:  " + inspected.getMana() + " / " + inspected.getBaseMana());
 			if (inspected.has("outfit")) {
-				st.append("\nOutfit:       " + inspected.get("outfit"));
+				st.append("\nOutfit:  " + inspected.get("outfit"));
 			} else if (inspected.has("class")) {
-				st.append("\nOutfit:       " + inspected.get("class"));
+				st.append("\nOutfit:  " + inspected.get("class"));
 			}
-			st.append("\nequips");
+			st.append("\n---------------------------");
+			st.append("\nWyposażenie");
 
 			for (final RPSlot slot : inspected.slots()) {
 				// showing these is either irrelevant, private, or spams too much
@@ -107,6 +107,7 @@ public class InspectAction extends AdministrationAction {
 						|| slot.getName().equals("!kills")) {
 					continue;
 				}
+				st.append("\n---------------------------");
 				st.append("\n    Slot " + slot.getName() + ": ");
 
 				if (slot.getName().startsWith("!")) {
