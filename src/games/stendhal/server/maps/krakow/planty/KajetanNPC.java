58dd5e52a86769ec807e5e8aedcad15a88c1cdd8
ZEKKEQ <zekkeq@o2.pl>
2019-07-13 15:18:56 +0200

    Możliwość kupowania kozy u Kajetana oraz sprzedania jej u Targona

--- src/games/stendhal/server/maps/krakow/planty/KajetanNPC.java
+++ src/games/stendhal/server/maps/krakow/planty/KajetanNPC.java
@@ -11,15 +11,24 @@
  ***************************************************************************/
 package games.stendhal.server.maps.krakow.planty;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import games.stendhal.common.grammar.ItemParserResult;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.core.rp.StendhalRPAction;
+import games.stendhal.server.entity.creature.Goat;
+import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
+import games.stendhal.server.entity.player.Player;
 
 /**
  * Build a NPC
@@ -27,6 +36,7 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
  * @author KarajuSs
  */
 public class KajetanNPC implements ZoneConfigurator {
+	public static final int BUYING_PRICE = 60;
 
 	/**
 	 * Configure a zone.
@@ -64,18 +74,61 @@ public class KajetanNPC implements ZoneConfigurator {
 
 			@Override
 			protected void createDialog() {
+				class GoatSellerBehaviour extends SellerBehaviour {
+					GoatSellerBehaviour(final Map<String, Integer> items) {
+						super(items);
+					}
+
+					@Override
+					public boolean transactAgreedDeal(ItemParserResult res, final EventRaiser seller, final Player player) {
+						if (res.getAmount() > 1) {
+							seller.say("Hmm... Nie sądzę, abyś mógł zaopiekować się więcej niż jedną kozą naraz.");
+							return false;
+						} else if (!player.hasGoat()) {
+							if (!player.drop("money", getCharge(res,player))) {
+								seller.say("Nie masz tyle pieniędzy.");
+								return false;
+							}
+							seller.say("Proszę bardzo, oto mała koza! Opiekuj się nią dobrze...");
+
+							final Goat goat = new Goat(player);
+							StendhalRPAction.placeat(seller.getZone(), goat, seller.getX(), seller.getY() + 1);
+
+							player.notifyWorldAboutChanges();
+
+							return true;
+						} else {
+							say("Dlaczego się nie upewnisz i nie poszukasz kozy, którą już masz?");
+							return false;
+						}
+					}
+				}
+
+				final Map<String, Integer> items = new HashMap<String, Integer>();
+				items.put("goat", BUYING_PRICE);
+
 				addGreeting("Witaj, co Cię do mnie sprowadza?");
 				addJob("Zajmuję się wypasaniem kóz.");
-				addOffer("Mogę Ci zaoferować kozę do wypasenia, gdy osiągnie ona wagę równą 100 to będziesz mógł ją sprzedać Targonowi.");
+				addOffer("Sprzedaję kozy. Aby kupić jedną wystarczy powiedzieć mi #'buy goat'. Jeżeli jesteś nowym w tym interesie to mogę Ci powiedzieć jak #podróżować z kozą, jak się nią #opiekować i powiem Ci, gdzie możesz ją #sprzedać. Jeżeli znajdziesz dziką kozę to możesz ją #przygarnąć.");
 				// zakup kozy: 60 money, karmi się trawą
 				addGoodbye();
+				new SellerAdder().addSeller(this, new GoatSellerBehaviour(items));
+				addReply(Arrays.asList("care", "opiekować"),
+						"Moja koza kocha jeść wysoką trawę. Stań obok, a twoja koza podejdzie i zacznie jeść. Możesz sprawdzić jej wagę klikając na nią prawym przyciskiem i wybierając 'Zobacz'. Jej waga będzie rosła wraz ze zjedzeniem każdej trawy.");
+				addReply(Arrays.asList("travel", "podróżować"),
+						"Gdy zmieniasz miejsce pobytu twoja koza powinna być blisko Ciebie, aby nie zginęła. Jeżeli nie zwraca na Ciebie uwagi wystarczy powiedzieć #goat aby ją wezwać. Jeżeli zdecydujesz się ją porzucić to kliknij na siebie prawym przyciskiem i wybierz 'Porzuć kozę', ale szczerze sądzę, że takie zachowanie jest odrażające.");
+				addReply(Arrays.asList("sell", "sprzedać"),
+						"Kiedy twoja koza osiągnie wagę 100 to możesz ja zabrać do Targona w Krakowie, a on kupi ją od Ciebie.");
+				addReply(Arrays.asList("own", "przygarnąć"),
+						"Jeżeli znajdziesz dziką lub porzuconą kozę to aby ją przygarnąć możesz kliknąć na nią prawym przyciskiem i wybrać 'Przygarnij'.");
 			}
 		};
 
 		npc.setDescription("Oto Kajetan. Zajmuje się wypasaniem kóz.");
-		npc.setEntityClass("noimagenpc"); // pasterz
+		npc.setEntityClass("seller3npc");
 		npc.setPosition(103, 110);
 		npc.initHP(100);
+		npc.setSounds(Arrays.asList("cough-11", "cough-2", "cough-3"));
 		zone.add(npc);
 	}
 }
--- src/games/stendhal/server/maps/krakow/planty/TargonNPC.java
+++ src/games/stendhal/server/maps/krakow/planty/TargonNPC.java
@@ -11,15 +11,31 @@
  ***************************************************************************/
 package games.stendhal.server.maps.krakow.planty;
 
+import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import games.stendhal.common.Rand;
+import games.stendhal.common.grammar.ItemParserResult;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.core.rp.StendhalRPAction;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.creature.Goat;
+import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.krakow.planty.KajetanNPC;
+import games.stendhal.server.util.Area;
+import marauroa.common.game.RPObject;
 
 /**
  * Build a NPC
@@ -27,6 +43,120 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
  * @author KarajuSs
  */
 public class TargonNPC implements ZoneConfigurator {
+	// The goat pen where Targon moves what he buys
+	/** Left X coordinate of the goat pen */ 
+	private static final int GOAT_PEN_X = 105;
+	/** Top Y coordinate of the goat pen */
+	private static final int GOAT_PEN_Y = 23;
+	/** Width of the goat pen */
+	private static final int GOAT_PEN_WIDTH = 16;
+	/** Height of the goat pen */
+	private static final int GOAT_PEN_HEIGHT = 6;
+	/** 
+	 * The maximum number of goat Targon keeps in his goat pen.
+	 */ 
+	private static final int MAX_GOAT_IN_PEN = 8;
+	/** The area covering the goat pen in Krakow */
+	private Area pen;
+
+	public class GoatBuyerSpeakerNPC extends SpeakerNPC {
+		public GoatBuyerSpeakerNPC(String name) {
+			super(name);
+			// HP needs to be > 0 for Targon to appear in the killer list for
+			// the wielki zły wilk
+			setBaseHP(100);
+			setHP(100);
+		}
+		/**
+		 * Get the area of the goat pen where bought goat 
+		 * should be moved.
+		 * 
+		 * @param zone the zone of the goat pen
+		 * @return area of the goat pen
+		 */
+		private Area getPen(StendhalRPZone zone) {
+			if (pen == null) {
+				Rectangle2D rect = new Rectangle2D.Double();
+				rect.setRect(GOAT_PEN_X, GOAT_PEN_Y, GOAT_PEN_WIDTH, GOAT_PEN_HEIGHT);
+				pen = new Area(zone, rect);
+			}
+			
+			return pen;
+		}
+		
+		/**
+		 * Try to get rid of the big bad wolf that could have spawned in the pen,
+		 * but miss it sometimes.
+		 *  
+		 * @param zone the zone to check
+		 */
+		private void killWolves(StendhalRPZone zone) {
+			if (Rand.throwCoin() == 1) { 
+				for (RPObject obj : zone) {
+					if (obj instanceof Creature) {
+						Creature wolf = (Creature) obj;
+						
+						if ("wilk".equals(wolf.get("subclass")) && getPen(zone).contains(wolf)) {
+							wolf.onDamaged(this, wolf.getHP());
+								return;
+						}
+					}
+				}
+			}
+		}
+		
+		/**
+		 * Get a list of the goat in the pen.
+		 * 
+		 * @param zone the zone to check
+		 * @return list of goat in the pen
+		 */
+		private List<Goat> goatInPen(StendhalRPZone zone) {
+			List<Goat> goat = new LinkedList<Goat>();
+			Area pen = getPen(zone);
+			
+			for (RPEntity entity : zone.getPlayerAndFriends()) {
+				if (entity instanceof Goat) {
+					if (pen.contains(entity)) {
+						goat.add((Goat) entity);
+					}
+				}
+			}
+			
+			return goat;
+		}
+		
+		/**
+		 * Move a bought goat to the den if there's space, or remove it 
+		 * from the zone otherwise. Remove old goat if there'd be more
+		 * than <code>MAX_GOAT_IN_PEN</code> after the addition.
+		 * 
+		 * @param goat the goat to be moved
+		 */
+		public void moveGoat(Goat goat) {
+			// The area of the goat den.
+			int x = Rand.randUniform(GOAT_PEN_X, GOAT_PEN_X + GOAT_PEN_WIDTH - 1);
+			int y = Rand.randUniform(GOAT_PEN_Y, GOAT_PEN_Y + GOAT_PEN_HEIGHT - 1);
+			StendhalRPZone zone = goat.getZone();
+			List<Goat> oldGoat = goatInPen(zone);
+			
+			killWolves(zone);
+			/*
+			 * Keep the amount of goat reasonable. Targon
+			 * a business man and letting the goat starve 
+			 * would be bad for busines.
+			 */
+			if (oldGoat.size() >= MAX_GOAT_IN_PEN) {
+				// Sato sells the oldest goat
+				zone.remove(oldGoat.get(0));
+			}
+			
+			if (!StendhalRPAction.placeat(zone, goat, x, y)) {
+				// there was no room for the goat. Simply eat it
+				goat.getZone().remove(goat);  
+			}
+		}
+	}
 
 	/**
 	 * Configure a zone.
@@ -40,8 +170,7 @@ public class TargonNPC implements ZoneConfigurator {
 	}
 
 	private void buildNPC(final StendhalRPZone zone) {
-		final SpeakerNPC npc = new SpeakerNPC("Targon") {
-
+		final SpeakerNPC npc = new GoatBuyerSpeakerNPC("Targon") {
 			@Override
 			protected void createPath() {
 				final List<Node> nodes = new LinkedList<Node>();
@@ -59,17 +188,76 @@ public class TargonNPC implements ZoneConfigurator {
 			@Override
 			protected void createDialog() {
 				addGreeting();
-				addJob("Zajmuję się handlem kóz.");
+				addJob("Skupuję kozy w Krakowie, ogólnie zajmuje się handlem kóz.");
+				addHelp("Skupuję kozy i sądzę, że to uczciwa cena. Jeżeli się zdecydujesz na sprzedaż to powiedz #'sprzedam goat', a zrobimy interes!");
 				addOffer("Jeżeli przyniesiesz mi kozę, która została przez Ciebie wypasiona to zostaniesz odpowiednio wynagrodzony.");
-				// 150 money za kozę ważącą 100 kg
+				// 350 money za kozę ważącą 100 kg
 				addGoodbye();
 			}
 		};
 
-		npc.setDescription("Oto Targon. Kupuje tylko w pełni wypasione owce i płaci za nie całkiem dobrze.");
-		npc.setEntityClass("noimagenpc"); // npctargon
+		final Map<String, Integer> buyitems = new HashMap<String, Integer>();
+		buyitems.put("goat", 350);
+		new BuyerAdder().addBuyer(npc, new GoatBuyerBehaviour(buyitems), true);
+		npc.setDescription("Oto Targon. Kupuje tylko w pełni wypasione kozy i płaci za nie całkiem dobrze.");
+		npc.setEntityClass("buyernpc");
 		npc.setPosition(98, 32);
-		npc.initHP(100);
+		npc.setSounds(Arrays.asList("hiccup-1", "sneeze-1"));
 		zone.add(npc);
+	}
+
+	private static class GoatBuyerBehaviour extends BuyerBehaviour {
+		GoatBuyerBehaviour(final Map<String, Integer> items) {
+			super(items);
+		}
+
+		private int getValue(ItemParserResult res, final Goat goat) {
+			return Math.round(getUnitPrice(res.getChosenItemName()) * ((float) goat.getWeight() / (float) Goat.MAX_WEIGHT));
+		}
+
+		@Override
+		public int getCharge(ItemParserResult res, final Player player) {
+			if (player.hasGoat()) {
+				final Goat goat = player.getGoat();
+				return getValue(res, goat);
+			} else {
+				// npc's answer was moved to BuyerAdder. 
+				return 0;
+			}
+		}
+
+		@Override
+		public boolean transactAgreedDeal(ItemParserResult res, final EventRaiser seller, final Player player) {
+			// res.getAmount() is currently ignored.
+
+			final Goat goat = player.getGoat();
+
+			if (goat != null) {
+				if (seller.getEntity().squaredDistance(goat) > 5 * 5) {
+					seller.say("Nie widzę stąd tej kozy! Przyprowadź ją bliżej, abym mógł sprawdzić.");
+				} else if (getValue(res, goat) < KajetanNPC.BUYING_PRICE) {
+					// prevent newbies from selling their goat too early
+					seller.say("Ta koza wygląda na zbyt chudą. Nakarm ją trawą i wróć, gdy będzie grubsza.");
+				} else {
+					seller.say("Dziękuję! Oto twoje pieniądze.");
+					payPlayer(res, player);
+					player.removeGoat(goat);
+
+					player.notifyWorldAboutChanges();
+					if(seller.getEntity() instanceof GoatBuyerSpeakerNPC) {
+						((GoatBuyerSpeakerNPC)seller.getEntity()).moveGoat(goat);
+					} else {
+						// only to prevent that an error occurs and the goat does not disappear
+						goat.getZone().remove(goat);
+					}
+
+					return true;
+				}
+			} else {
+				seller.say(player.getTitle() + " nie posiadasz kozy! W co próbujesz pogrywać?");
+			}
+
+			return false;
+		}
 	}
 }
