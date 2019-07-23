67deb03e8f5da5003ab89cbb3fa9ed1e51c60497
ZEKKEQ <zekkeq@o2.pl>
2019-07-23 03:11:10 +0200

    Update HadrinNPC.java

--- src/games/stendhal/server/maps/krakow/blacksmith/HadrinNPC.java
+++ src/games/stendhal/server/maps/krakow/blacksmith/HadrinNPC.java
@@ -11,22 +11,15 @@
  ***************************************************************************/
 package games.stendhal.server.maps.krakow.blacksmith;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.behaviour.adder.ProducerAdder;
-import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
-import games.stendhal.server.entity.npc.behaviour.impl.ProducerBehaviour;
-import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
-import games.stendhal.server.core.engine.SingletonRepository;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * @author zekkeq
@@ -58,35 +51,11 @@ public class HadrinNPC implements ZoneConfigurator {
 
 			@Override
 			protected void createDialog() {
-			new SellerAdder().addSeller(this, new SellerBehaviour(SingletonRepository.getShopList().get("sellkopalnia")));
-
-			// Xoderos casts iron if you bring him wood and iron ore.
-			final Map<String, Integer> requiredResources = new TreeMap<String, Integer>();	
-			requiredResources.put("polano", 2);
-			requiredResources.put("bryłka złota", 1);
-			requiredResources.put("piórko", 1);
-			requiredResources.put("money", 2);
-
-			final ProducerBehaviour behaviour = new ProducerBehaviour("krasnolud_cast_arrow",
-				Arrays.asList("make", "zrób"), "strzała złota", requiredResources, 1 * 60);
-
-			new ProducerAdder().addProducer(this, behaviour,
-				"Witaj! Mogę zrobić dla ciebie złote strzały, a może interesuje cię moja #oferta specjalna? Powiedz tylko #'zrób'.");
 				addGreeting();
-				addJob("Produkuję strzały do łuków.");
-
-				addReply("polano",
-						"Potrzebuję drewna na promień do strzały. Porozmawiaj z drwalem on ci powie gdzie można ścinać drzewa.");
-				addReply(Arrays.asList("gold nugget", "nugget", "gold", "złota", "bryłka","bryłka złota"),
-						"Bryłki złota znajdziesz w Zakopane koło źródeł na wschód od domku poniżej jaskini zbójników. Potrzebuję je na groty.");
-				addReply("piórko",
-						"Potrzebuję je na lotki. Zabij kilka gołębi.");
-				addReply("kilof",
-						"Przydatny przy wydobyciu siarki i węgla.");
-
+				addJob("Zajmuje się ulepszaniem złotych ciupag.");
 				addReply("łopata", "No cóż... czymś trzeba kopać.");
 				addReply("lina", "Przydatna gdy zechcesz zejść na niższe poziomy.");
-				addHelp("Jeśli przyniesiesz mi #'polano', #'bryłka złota' i #'piórko', mogę zrobić dla ciebie złote strzały. Powiedz tylko #'zrób strzała złota'.");
+				addHelp("Jeżeli będziesz chciał to mógłbym ulepszyć Twą złotą ciupagę.");
 				addGoodbye();
 			}
 		};
