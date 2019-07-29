9c8ea55988b674fd3f641ffef7d8f1e0f8acd55e
ZEKKEQ <zekkeq@o2.pl>
2019-07-29 04:40:01 +0200

    Revert "Revert"
    This reverts commit 6bd13fe3f8280c0c37cd4525bdc6e4124703e4a6.

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-POL_VERSION="1.02.1"
+POL_VERSION="1.02.2"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:polanieonline-server-$POL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
 java -Xmx400m -cp "${LOCALCLASSPATH}" games.stendhal.server.StendhalServer -c server.ini -l
+
