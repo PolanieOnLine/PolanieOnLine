6bd13fe3f8280c0c37cd4525bdc6e4124703e4a6
ZEKKEQ <zekkeq@o2.pl>
2019-07-29 04:38:13 +0200

    Revert

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-POL_VERSION="1.02.2"
+POL_VERSION="1.02.1"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:polanieonline-server-$POL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
 java -Xmx400m -cp "${LOCALCLASSPATH}" games.stendhal.server.StendhalServer -c server.ini -l
-
