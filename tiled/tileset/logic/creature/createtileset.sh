e4a079e061bcb21bf8f18f2ed5c85c851d08a11b
ZEKKEQ <zekkeq@o2.pl>
2019-10-15 17:40:21 +0200

    Update createalltilesformonsters.sh

--- tiled/tileset/logic/creature/createtileset.sh
+++ tiled/tileset/logic/creature/createtileset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for img in `ls $1/*.png` 
+for img in `ls $1/*.png`
 do
   convert $img -crop $2 $img.tmp.png
   convert $img.tmp.png -trim +repage -resize "30x30>" -size 30x30 xc:white +swap -gravity center -composite -bordercolor black -border 1x1 $img.2.tmp.png
