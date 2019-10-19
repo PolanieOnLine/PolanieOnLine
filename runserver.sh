14b123238b2e2bd222a809b935ded7c6d25da74a
ZEKKEQ <zekkeq@o2.pl>
2019-10-19 22:45:36 +0200

    włączenie eventów

--- runserver.sh
+++ runserver.sh
@@ -3,5 +3,4 @@ POL_VERSION="1.05.5"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:polanieonline-server-$POL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
-java -Xmx400m -cp "${LOCALCLASSPATH}" games.stendhal.server.StendhalServer -c server.ini -l
-
+java -Xmx400m -Dstendhal.minetown -Dpol.morexp -cp "${LOCALCLASSPATH}" games.stendhal.server.StendhalServer -c server.ini -l
