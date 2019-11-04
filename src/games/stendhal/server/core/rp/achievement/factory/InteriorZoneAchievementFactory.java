f2797183135cf16d4ee4db2f2890ef014203f445
ZEKKEQ <zekkeq@o2.pl>
2019-11-04 01:57:47 +0100

    Przerobiono osiągnięcie za zwiedzenie budynków w zako

--- src/games/stendhal/server/core/rp/achievement/factory/InteriorZoneAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/InteriorZoneAchievementFactory.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 
 import games.stendhal.server.core.rp.achievement.Achievement;
 import games.stendhal.server.core.rp.achievement.Category;
+import games.stendhal.server.entity.npc.condition.PlayerVisitedZonesCondition;
 import games.stendhal.server.entity.npc.condition.PlayerVisitedZonesInRegionCondition;
 /**
  * Factory for interior zone achievements
@@ -43,12 +44,18 @@ public class InteriorZoneAchievementFactory extends AbstractAchievementFactory {
 		list.add(createAchievement("zone.interior.wieliczkacity", "Miasto Wieliczka", "Odwiedził wszystkie pomieszczenia w regionie Wieliczki",
 									Achievement.MEDIUM_BASE_SCORE, true,
 									new PlayerVisitedZonesInRegionCondition("wieliczka", Boolean.FALSE, Boolean.FALSE)));
-		list.add(createAchievement("zone.interior.zakopanecity", "Miasto Zakopane", "Odwiedził wszystkie pomieszczenia w regionie Zakopanego",
-									Achievement.MEDIUM_BASE_SCORE, true,
-									new PlayerVisitedZonesInRegionCondition("zakopane", Boolean.FALSE, Boolean.FALSE)));
 		list.add(createAchievement("zone.interior.gdanskcity", "Miasto Gdańsk", "Odwiedził wszystkie pomieszczenia w regionie Gdańska",
 									Achievement.MEDIUM_BASE_SCORE, true,
 									new PlayerVisitedZonesInRegionCondition("gdansk", Boolean.FALSE, Boolean.FALSE)));
+
+		list.add(createAchievement("zone.interior.zakopanecity", "Miasto Zakopane", "Odwiedził wszystkie pomieszczenia w regionie Zakopanego",
+									Achievement.MEDIUM_BASE_SCORE, true,
+									new PlayerVisitedZonesCondition("int_zakopane_home_0", "int_zakopane_home_-1", "int_zakopane_townhall", "int_zakopane_postoffice",
+											"int_zakopane_postoffice", "int_zakopane_bank_0", "int_zakopane_bank_1", "int_zakopane_bat_tower_-1", "int_zakopane_bat_tower_0",
+											"int_zakopane_bat_tower_1", "int_zakopane_bat_tower_2", "int_zakopane_bat_tower_3", "int_zakopane_blacksmith", "int_zakopane_hostel",
+											"int_zakopane_hospital", "int_zakopane_seller_house", "int_zakopane_mill_0", "int_zakopane_playroom", "int_zakopane_shop",
+											"int_zakopane_stable", "int_zakopane_tavern", "int_zakopane_church", "int_zakopane_chapel", "int_zakopane_trainstation",
+											"int_zakopane_mountain_room", "int_zakopane_bakery", "int_zakopane_maisonette_0", "int_zakopane_maisonette_1", "int_zakopane_dobromir_house")));
 		return list;
 	}
 
