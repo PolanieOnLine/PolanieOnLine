7956ab4917ef7f09adb929b4a7b768af40004f6a
ZEKKEQ <zekkeq@o2.pl>
2020-01-08 23:43:41 +0100

    Update RebornAchievementFactory.java

--- src/games/stendhal/server/core/rp/achievement/factory/RebornAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/RebornAchievementFactory.java
@@ -13,34 +13,24 @@ public class RebornAchievementFactory extends AbstractAchievementFactory {
 	private static final String QUEST_SLOT = "reset_level";
 
 	@Override
-	public Collection<Achievement> createAchievements() {
-		List<Achievement> achievements = new LinkedList<Achievement>();
-
-		// first reborn
-		achievements.add(createAchievement("reborn.quest.first", "Nowo narodzony", "Narodził się na nowo",
-			Achievement.EASY_BASE_SCORE, true, new QuestCompletedCondition(QUEST_SLOT)));
-
-		// second reborn
-		achievements.add(createAchievement("reborn.quest.second", "Nowy ja, nowa przygoda", "Narodził się na nowo po raz drugi",
-			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 1)));
-
-		// third reborn
-		achievements.add(createAchievement("reborn.quest.third", "Przygodo... ruszam!", "Narodził się na nowo po raz trzeci",
-			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 2)));
-
-		// fourth reborn
-		achievements.add(createAchievement("reborn.quest.fourth", "Czwarta powtórka przygody", "Narodził się na nowo po raz czwarty",
-			Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 3)));
-
-		// fifth reborn
-		achievements.add(createAchievement("reborn.quest.fifth", "Tym razem historia się nie powtórzy", "Narodził się na nowo po raz ostatni",
-			Achievement.LEGENDARY_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 4)));
-
-		return achievements;
+	protected Category getCategory() {
+		return Category.REBORN;
 	}
 
 	@Override
-	protected Category getCategory() {
-		return Category.REBORN;
+	public Collection<Achievement> createAchievements() {
+		List<Achievement> achievements = new LinkedList<Achievement>();
+		achievements.add(createAchievement("quest.special.reborn.1", "Nowo narodzony", "Narodził się na nowo",
+			Achievement.EASY_BASE_SCORE, true, new QuestCompletedCondition(QUEST_SLOT)));
+		achievements.add(createAchievement("quest.special.reborn.2", "Nowy ja, nowa przygoda", "Narodził się na nowo po raz drugi",
+			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 1)));
+		achievements.add(createAchievement("quest.special.reborn.3", "Przygodo... ruszam!", "Narodził się na nowo po raz trzeci",
+			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 2)));
+		achievements.add(createAchievement("quest.special.reborn.4", "Powtórna przygoda", "Narodził się na nowo po raz czwarty",
+			Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 3)));
+		achievements.add(createAchievement("quest.special.reborn.5", "Tym razem historia się nie powtórzy", "Narodził się na nowo po raz ostatni",
+			Achievement.LEGENDARY_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 4)));
+
+		return achievements;
 	}
 }
