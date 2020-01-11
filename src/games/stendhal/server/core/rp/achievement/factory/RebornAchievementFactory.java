21f4f3ed959b941741cefd719180972c5de6fbf5
ZEKKEQ <zekkeq@o2.pl>
2020-01-11 03:59:20 +0100

    Poprawiono indeks

--- src/games/stendhal/server/core/rp/achievement/factory/RebornAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/RebornAchievementFactory.java
@@ -23,13 +23,13 @@ public class RebornAchievementFactory extends AbstractAchievementFactory {
 		achievements.add(createAchievement("quest.special.reborn.1", "Nowo narodzony", "Narodził się na nowo",
 			Achievement.EASY_BASE_SCORE, true, new QuestCompletedCondition(QUEST_SLOT)));
 		achievements.add(createAchievement("quest.special.reborn.2", "Nowy ja, nowa przygoda", "Narodził się na nowo po raz drugi",
-			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 1)));
+			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 1, 1)));
 		achievements.add(createAchievement("quest.special.reborn.3", "Przygodo... ruszam!", "Narodził się na nowo po raz trzeci",
-			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 2)));
+			Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 1, 2)));
 		achievements.add(createAchievement("quest.special.reborn.4", "Powtórna przygoda", "Narodził się na nowo po raz czwarty",
-			Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 3)));
+			Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 1, 3)));
 		achievements.add(createAchievement("quest.special.reborn.5", "Tym razem historia się nie powtórzy", "Narodził się na nowo po raz ostatni",
-			Achievement.LEGENDARY_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 2, 4)));
+			Achievement.LEGENDARY_BASE_SCORE, true, new QuestStateGreaterThanCondition(QUEST_SLOT, 1, 4)));
 
 		return achievements;
 	}
