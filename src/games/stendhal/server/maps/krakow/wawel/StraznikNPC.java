767ebf9b3d200a4b87b028dade63152b937d3425
ZEKKEQ <zekkeq@o2.pl>
2020-01-12 00:55:06 +0100

    Przeniesiono konfigurator Strażnika z XML do osobnej klasy + dodano alternatywny sprite NPC

--- src/games/stendhal/server/maps/krakow/wawel/StraznikNPC.java
+++ src/games/stendhal/server/maps/krakow/wawel/StraznikNPC.java
@@ -11,23 +11,49 @@
  ***************************************************************************/
 package games.stendhal.server.maps.krakow.wawel;
 
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
 
 /**
  * A young lady (original name: Straznik) who heals players without charge.
  */
-public class StraznikNPC extends SpeakerNPCFactory {
+public class StraznikNPC implements ZoneConfigurator {
 
 	@Override
-	public void createDialog(final SpeakerNPC npc) {
-		npc.addGreeting("Witaj wędrowcze.");
-		npc.addJob("Prowadzę spokojne życie. Pilnuje #wejścia na wawel.");
-		npc.addHelp("Mogę sprzedać ci zwój krakowski lub zwój tatrzański.");
-		new SellerAdder().addSeller(npc, new SellerBehaviour(SingletonRepository.getShopList().get("wawel")));
-		npc.addGoodbye();
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildguardian(zone);
+	}
+
+	private void buildguardian(final StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Strażnik") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Witaj wędrowcze.");
+				addJob("Prowadzę spokojne życie. Pilnuje #wejścia na wawel.");
+				addHelp("Mogę sprzedać ci zwój krakowski lub zwój tatrzański.");
+				new SellerAdder().addSeller(this, new SellerBehaviour(SingletonRepository.getShopList().get("wawel")));
+				addGoodbye();
+			}
+		};
+
+		npc.setEntityClass("transparentnpc");
+		npc.setAlternativeImage("guardian");
+		npc.setDescription("Oto Strażnik. Chroni bramę Wawela przed słabymi i niegodnymi wojownikami.");
+		npc.setPosition(102, 82);
+		npc.initHP(100);
+		npc.put("no_shadow", "");
+		zone.add(npc);
 	}
 }
