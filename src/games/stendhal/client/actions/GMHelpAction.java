a0e9fac876a4e2bfce8a2d236fc2de99ac5ed55f
ZEKKEQ <zekkeq@o2.pl>
2020-05-02 00:51:49 +0200

    Wrzuta poprawek

--- src/games/stendhal/client/actions/GMHelpAction.java
+++ src/games/stendhal/client/actions/GMHelpAction.java
@@ -52,6 +52,8 @@ class GMHelpAction implements SlashAction {
 				"\t\tUmożliwia aktualizację powodu zamknięcia wybranego #wojownika.",
 				"- /inspect <wojownik>",
 				"\t\tPokazuje kompletne informacje o #wojowniku.",
+				"- /inspectkill <wojownik> <stworzenie>",
+				"\t\tPokazuje ilość zabójstw #potwora u #wojownika.",
 				"- /inspectquest <wojownik> <nazwa_zadania>",
 				"\t\tPokazuje stan zadania #wojownika.",
 				"- /script <nazwaskryptu>",
