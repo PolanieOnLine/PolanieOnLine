d55b46d54d964ece7176b2d75958c6680a9ca806
ZEKKEQ <zekkeq@o2.pl>
2020-05-03 12:04:21 +0200

    Zmieniono nazwy z "karmazynowy hełm" na "hełm karmazynowy"

--- src/games/stendhal/server/core/rp/achievement/factory/ItemAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/ItemAchievementFactory.java
@@ -63,7 +63,7 @@ public class ItemAchievementFactory extends AbstractAchievementFactory {
 
 		itemAchievements.add(createAchievement("item.set.red", "Niebezpieczna Amazonia", "Zdobył cały karmazynowy zestaw",
 				Achievement.MEDIUM_BASE_SCORE, true,
-				new PlayerLootedNumberOfItemsCondition(1, "zbroja karmazynowa", "karmazynowy hełm", "płaszcz karmazynowy",
+				new PlayerLootedNumberOfItemsCondition(1, "zbroja karmazynowa", "hełm karmazynowy", "płaszcz karmazynowy",
 						"spodnie karmazynowe", "buty karmazynowe", "tarcza karmazynowa", "rękawice karmazynowe", "pas karmazynowy")));
 
 		itemAchievements.add(createAchievement("item.set.blue", "Czuję Błękit", "Zdobył cały lazurowy zestaw",
