56c7a6c686424e7f90a1ba9a263dc78f34e4012d
ZEKKEQ <zekkeq@o2.pl>
2020-05-03 11:45:06 +0200

    Revert "Update HealerAdder.java"
    This reverts commit 2cec3391d6c1ddc87c4d6fdecbae2fb9f1bac655.

--- src/games/stendhal/server/entity/npc/behaviour/adder/HealerAdder.java
+++ src/games/stendhal/server/entity/npc/behaviour/adder/HealerAdder.java
@@ -30,7 +30,7 @@ import games.stendhal.server.entity.player.Player;
 
 public class HealerAdder {
 
-	private final ServicersRegister servicersRegister = SingletonRepository.getServicersRegister();
+    private final ServicersRegister servicersRegister = SingletonRepository.getServicersRegister();
 
 	/**
 	 * Behaviour parse result in the current conversation.
@@ -50,132 +50,103 @@ public class HealerAdder {
 	 * unless they are very new to the game.
 	 *
 	 * @param npc
-	 * 		SpeakerNPC
+	 *            SpeakerNPC
 	 * @param cost
-	 * 		The price which can be positive for a lump sum cost, 0 for free healing
-	 * 		or negative for a price dependent on level of player.
+	 *            The price which can be positive for a lump sum cost, 0 for free healing
+	 *            or negative for a price dependent on level of player.
 	 */
 	public void addHealer(final SpeakerNPC npc, final int cost) {
 		final HealerBehaviour healerBehaviour = new HealerBehaviour(cost);
 		servicersRegister.add(npc.getName(), healerBehaviour);
 
-		final ChatAction calculateCostAction = new ChatAction() {
-			@Override
-			public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-				ItemParserResult res = new ItemParserResult(true, "heal", 1, null);
-
-				int cost = healerBehaviour.getCharge(res, player);
-				currentBehavRes = res;
-
-				String badboymsg = "";
-				if (player.isBadBoy()) {
-					cost = cost * 2;
-					currentBehavRes.setAmount(2);
-					badboymsg = " Leczenie tych co zabili innych kosztuję więcej.";
-				}
-
-				if (cost > 0) {
-					raiser.say("Leczenie kosztuje " + cost
-							+ "." + badboymsg + " Posiadasz tyle?");
-
-					raiser.setCurrentState(ConversationStates.HEAL_OFFERED); // success
-				} else if (cost < 0) {
-					// price depends on level if cost was set at -1
-					// where the factor is |cost| and we have a +1
-					// to avoid 0 charge.
-					cost = player.getLevel() * Math.abs(cost) + 1;
-					raiser.say("Uleczenie kogoś z Twoimi zdolnościami kosztuje "
-							+ cost
-							+ " money." + badboymsg + " Posiadasz tyle?");
-
-					raiser.setCurrentState(ConversationStates.HEAL_OFFERED); // success
-				} else {
-					if ((player.getAtk() > 35) || (player.getDef() > 35)) {
-						raiser.say("Przepraszam, ale nie mogę Cię uleczyć ponieważ jesteś zbyt potężny jak na moje możliwości");
-					} else if ((!player.isNew()
-							&& (player.getLastPVPActionTime() > System
-									.currentTimeMillis()
-									- 2
-									* MathHelper.MILLISECONDS_IN_ONE_HOUR) || player.isBadBoy())) {
-						// ignore the PVP flag for very young
-						// characters
-						// (low atk, low def AND low level)
-						raiser.say("Przepraszam, ale posiadasz złą aurę i teraz nie mogę Ciebie uleczyć.");
-					} else {
-						raiser.say("Zostałeś uleczony. W czym jeszcze mogę pomóc?");
-						healerBehaviour.heal(player);
-					}
-				}
-			}
-		};
-
-		final ChatAction healAction = new ChatAction() {
-			@Override
-			public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-				int cost = healerBehaviour.getCharge(currentBehavRes, player);
-				if (cost < 0) {
-					cost = player.getLevel() * Math.abs(cost) + 1;
-				}
-				if (player.drop("money", cost)) {
-					healerBehaviour.heal(player);
-					raiser.say("Zostałeś uleczony. W czym jeszcze mogę pomóc?");
-				} else {
-					raiser.say("Przepraszam, ale nie możesz sobie na to pozwolić.");
-				}
-
-				currentBehavRes = null;
-			}
-		};
-
-		addHealer(npc, calculateCostAction, healAction);
-	}
-
-	/**
-	 * Makes the NPC a healer.
-	 *
-	 * @param npc
-	 * 		SpeakerNPC who does the healing.
-	 * @param calculateCostAction
-	 * 		Action to take to determine cost.
-	 * @param healAction
-	 * 		Action to take when player is healed.
-	 */
-	public void addHealer(final SpeakerNPC npc, final ChatAction calculateCostAction, final ChatAction healAction) {
 		// Give attribute to healers
-		npc.put("job_healer", "");
+		npc.put("job_healer", 0);
 
 		final Engine engine = npc.getEngine();
 
 		engine.add(ConversationStates.ATTENDING,
-				ConversationPhrases.OFFER_MESSAGES,
-				null,
-				false,
-				ConversationStates.ATTENDING,
-				"Mogę #wyleczyć poniesione rany w walce.",
-				null);
+				ConversationPhrases.OFFER_MESSAGES, null,
+				false, ConversationStates.ATTENDING, "Mogę Cię uleczyć. Powiedz tylko #ulecz.", null);
 
-		engine.add(ConversationStates.ATTENDING,
-				Arrays.asList("heal", "ulecz", "wylecz"),
-				null,
-				false,
-				ConversationStates.ATTENDING,
-				null,
-				calculateCostAction);
+		engine.add(ConversationStates.ATTENDING, Arrays.asList("heal", "ulecz", "wylecz"), null,
+				false, ConversationStates.ATTENDING,
+				null, new ChatAction() {
+					@Override
+					public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+						ItemParserResult res = new ItemParserResult(true, "heal", 1, null);
+
+						int cost = healerBehaviour.getCharge(res, player);
+						currentBehavRes = res;
+
+						String badboymsg = "";
+						if (player.isBadBoy()) {
+							cost = cost * 2;
+							currentBehavRes.setAmount(2);
+							badboymsg = " Leczenie tych co zabili innych kosztuję więcej.";
+						}
+
+						if (cost > 0) {
+							raiser.say("Leczenie kosztuje " + cost
+									+ "." + badboymsg + " Posiadasz tyle?");
+
+							raiser.setCurrentState(ConversationStates.HEAL_OFFERED); // success
+						} else if (cost < 0) {
+							// price depends on level if cost was set at -1
+							// where the factor is |cost| and we have a +1
+							// to avoid 0 charge.
+							cost = player.getLevel() * Math.abs(cost) + 1;
+							raiser.say("Uleczenie kogoś z Twoimi zdolnościami kosztuje "
+									+ cost
+									+ " money. Posiadasz tyle?");
+
+							raiser.setCurrentState(ConversationStates.HEAL_OFFERED); // success
+						} else {
+							if ((player.getAtk() > 35) || (player.getDef() > 35)) {
+								raiser.say("Przepraszam, ale nie mogę Cię uleczyć ponieważ jesteś zbyt potężny jak na moje możliwości");
+							} else if ((!player.isNew()
+									&& (player.getLastPVPActionTime() > System
+											.currentTimeMillis()
+											- 2
+											* MathHelper.MILLISECONDS_IN_ONE_HOUR) || player.isBadBoy())) {
+								// ignore the PVP flag for very young
+								// characters
+								// (low atk, low def AND low level)
+								raiser.say("Przepraszam, ale posiadasz złą aurę i teraz nie mogę Ciebie uleczyć.");
+							} else {
+								raiser.say("Zostałeś uleczony. W czym jeszcze mogę pomóc?");
+								healerBehaviour.heal(player);
+							}
+						}
+
+					}
+		});
 
 		engine.add(ConversationStates.HEAL_OFFERED,
-				ConversationPhrases.YES_MESSAGES,
-				null,
-				false,
-				ConversationStates.ATTENDING,
-				null,
-				healAction);
+				ConversationPhrases.YES_MESSAGES, null,
+				false, ConversationStates.ATTENDING,
+				null, new ChatAction() {
+					@Override
+					public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+						int cost = healerBehaviour.getCharge(currentBehavRes, player);
+						if (cost < 0) {
+							cost = player.getLevel() * Math.abs(cost) + 1;
+						}
+						if (player.drop("money",
+								cost)) {
+							healerBehaviour.heal(player);
+							raiser.say("Zostałeś uleczony. W czym jeszcze mogę pomóc?");
+						} else {
+							raiser.say("Przepraszam, ale nie możesz sobie na to pozwolić.");
+						}
+
+						currentBehavRes = null;
+					}
+				});
 
 		engine.add(ConversationStates.HEAL_OFFERED,
-				ConversationPhrases.NO_MESSAGES,
-				null,
-				false,
-				ConversationStates.ATTENDING,
-				"Dobrze w czym jeszcze mogę pomóc?",
-				null);
+				ConversationPhrases.NO_MESSAGES, null,
+				false, ConversationStates.ATTENDING,
+				"Dobrze w czym jeszcze mogę pomóc?", null);
 	}
+
 }
