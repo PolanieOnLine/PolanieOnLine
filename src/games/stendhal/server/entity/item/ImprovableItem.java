713d31f943eaf7c80b62681325296b6b55c50134
ZEKKEQ <45718921+ZEKKEQ@users.noreply.github.com>
2020-08-23 19:41:28 +0200

    Naprawiono limit ulepszeń

--- src/games/stendhal/server/entity/item/ImprovableItem.java
+++ src/games/stendhal/server/entity/item/ImprovableItem.java
@@ -2,11 +2,7 @@ package games.stendhal.server.entity.item;
 
 import java.util.Map;
 
-import games.stendhal.server.entity.RPEntity;
-import games.stendhal.server.entity.player.Player;
-
 public class ImprovableItem extends Item {
-	private boolean notified = false;
 
 	public ImprovableItem(String name, String clazz, String subclass, Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
@@ -25,35 +21,13 @@ public class ImprovableItem extends Item {
 	}
 
 	public void upgrade() {
-		if (getImprove() >= getMaxImproves()) {
-			put("improve", getImprove() + 1);
-			notified = false;
-		} else {
-			notified = true;
-		}
+		put("improve", maxImproves());
 	}
-
-	@Override
-	public void deteriorate() {
-		put("improve", 0);
-	}
-
-	@Override
-	public void deteriorate(final RPEntity player) {
-		deteriorate();
-
+	private int maxImproves() {
 		if (getImprove() == getMaxImproves()) {
-			onMaxImproves(player);
+			return getMaxImproves();
 		}
-	}
-
-	private void onMaxImproves(final RPEntity player) {
-		if (!notified) {
-			if (player instanceof Player) {
-				((Player) player).sendPrivateText("Twój przedmiot " + getName() + " jest już maksymalnie ulepszony.");
-				notified = true;
-			}
-		}
+		return getImprove() + 1;
 	}
 
 	@Override
