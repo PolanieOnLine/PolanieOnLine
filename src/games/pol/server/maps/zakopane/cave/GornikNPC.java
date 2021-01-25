1de2fa17d4d3ccd94792049226242f21c36868b0
KarajuSs <karajussgg@gmail.com>
2021-01-25 22:40:47 +0100

    Wprowadzono nowego NPC oraz postawiono chatkę w kopalni zakopca

---
+++ src/games/pol/server/maps/zakopane/cave/GornikNPC.java
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2020 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.pol.server.maps.zakopane.cave;
+
+import java.util.Map;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+/**
+ * @author KarajuSs
+ */
+public class GornikNPC implements ZoneConfigurator {
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	@Override
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(final StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Górnik") {
+			@Override
+			protected void createDialog() {
+				addGreeting();
+				addJob("");
+				addHelp("");
+				addGoodbye();
+			}
+
+			@Override
+			protected void onGoodbye(RPEntity player) {
+				setDirection(Direction.RIGHT);
+			}
+		};
+
+		npc.setDescription("Oto Górnik.");
+		npc.setEntityClass("npcgornik");
+		npc.setDirection(Direction.RIGHT);
+		npc.setPosition(18, 70);
+		zone.add(npc);
+	}
+}
