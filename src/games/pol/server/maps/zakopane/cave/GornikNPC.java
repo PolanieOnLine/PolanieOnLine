6d75174888e6cf310d7e921446549d4f34408c2d
KarajuSs <karajussgg@gmail.com>
2021-02-02 15:42:59 +0100

    Update GornikNPC.java

--- src/games/pol/server/maps/zakopane/cave/GornikNPC.java
+++ src/games/pol/server/maps/zakopane/cave/GornikNPC.java
@@ -66,7 +66,7 @@ public class GornikNPC implements ZoneConfigurator {
 			}
 		};
 
-		npc.setDescription("Oto Górnik. Może opowie nam o różnych kamieniach.");
+		npc.setDescription("Oto Górnik. Może opowie nam o różnych kilofach.");
 		npc.setEntityClass("npcgornik");
 		npc.setDirection(Direction.RIGHT);
 		npc.setPosition(18, 70);
