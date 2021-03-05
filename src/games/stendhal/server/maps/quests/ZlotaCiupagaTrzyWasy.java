84814001d9ec9f7b9d27f4a71e1b19aa5eca5719
KarajuSs <karajussgg@gmail.com>
2021-03-05 19:47:26 +0100

    Utrudniono zadanie na ciupagę z trzema wąsami, aby bardziej klasyfikowało się pod "legendarne"

--- src/games/stendhal/server/maps/quests/ZlotaCiupagaTrzyWasy.java
+++ src/games/stendhal/server/maps/quests/ZlotaCiupagaTrzyWasy.java
@@ -50,6 +50,10 @@ public class ZlotaCiupagaTrzyWasy extends AbstractQuest {
 	private static final String QUEST_SLOT = "ciupaga_trzy_wasy";
 
 	private static final String DWA_WASY_QUEST_SLOT = "ciupaga_dwa_wasy";
+	private static final String FRIEND_1 = "goralski_kolekcjoner3";
+	private static final String FRIEND_2 = "belts_collector";
+	private static final String FRIEND_3 = "gloves_collector";
+	private static final String FRIEND_4 = "krolewski_plaszcz";
 
 	private static Logger logger = Logger.getLogger(ZlotaCiupagaTrzyWasy.class);
 
@@ -67,35 +71,55 @@ public class ZlotaCiupagaTrzyWasy extends AbstractQuest {
 			new ChatAction() {
 				@Override
 				public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-					if (player.isQuestCompleted(DWA_WASY_QUEST_SLOT)) {
-						if(player.getLevel() >= 350) {
-							if(player.getKarma() >= 1500) {
-								if(player.hasKilled("azazel")) {
-									if (!player.hasQuest(QUEST_SLOT) || "rejected".equals(player.getQuest(QUEST_SLOT))) {
-										raiser.say("Musisz być dzielnym wojownikiem skoro dotarłeś aż tu. Mam dla ciebie zadanie, czy jesteś gotów?");
-									} else if (player.getQuest(QUEST_SLOT, 0).equals("start")) {
-										raiser.say("Już się Ciebie pytałem czy chcesz ulepszyć złotą ciupagę!");
-									} else if (player.isQuestCompleted(QUEST_SLOT)) {
-										raiser.say("Już ulepszyłem dla Ciebie złotą ciupagę.");
-										raiser.setCurrentState(ConversationStates.ATTENDING);
+					if (player.isQuestCompleted(FRIEND_1)) {
+						if (player.isQuestCompleted(FRIEND_2)) {
+							if (player.isQuestCompleted(FRIEND_3)) {
+								if (player.isQuestCompleted(FRIEND_4)) {
+									if (player.isQuestCompleted(DWA_WASY_QUEST_SLOT)) {
+										if(player.getLevel() >= 350) {
+											if(player.getKarma() >= 1500) {
+												if(player.hasKilled("azazel")) {
+													if (!player.hasQuest(QUEST_SLOT) || "rejected".equals(player.getQuest(QUEST_SLOT))) {
+														raiser.say("Musisz być dzielnym wojownikiem skoro dotarłeś aż tu. Mam dla ciebie zadanie, czy jesteś gotów?");
+													} else if (player.getQuest(QUEST_SLOT, 0).equals("start")) {
+														raiser.say("Już się Ciebie pytałem czy chcesz ulepszyć złotą ciupagę!");
+													} else if (player.isQuestCompleted(QUEST_SLOT)) {
+														raiser.say("Już ulepszyłem dla Ciebie złotą ciupagę.");
+														raiser.setCurrentState(ConversationStates.ATTENDING);
+													} else {
+														raiser.say("Dlaczego zawracasz mi głowę skoro nie ukończyłeś zadania?");
+														raiser.setCurrentState(ConversationStates.ATTENDING);
+													}
+												} else {
+													npc.say("Rozmawiam tylko z osobami, które wykazały się w walce zabijając azazela.");
+													raiser.setCurrentState(ConversationStates.ATTENDING);
+												}
+											} else {
+												npc.say("Twoja karma jest zbyt słaba aby podołać temu zadaniu. Postaraj się aby była 1500 lub więcej");
+												raiser.setCurrentState(ConversationStates.ATTENDING);
+											}
+										} else {
+											npc.say("Twój stan społeczny jest zbyt niski aby podjąć te zadanie. Wróć gdy zdobędziesz 350 poziom.");
+											raiser.setCurrentState(ConversationStates.ATTENDING);
+										}
 									} else {
-										raiser.say("Dlaczego zawracasz mi głowę skoro nie ukończyłeś zadania?");
+										npc.say("Nim mógłbym ulepszyć Tobie złotą ciupagę, wykonaj zadanie u Władcy Smoków!");
 										raiser.setCurrentState(ConversationStates.ATTENDING);
 									}
 								} else {
-									npc.say("Rozmawiam tylko z osobami, które wykazały się w walce zabijając azazela.");
+									npc.say("Jeśli pomożesz Królowi Krakowi, ja jako pierwszy o tym się dowiem.");
 									raiser.setCurrentState(ConversationStates.ATTENDING);
 								}
 							} else {
-								npc.say("Twoja karma jest zbyt słaba aby podołać temu zadaniu. Postaraj się aby była 1500 lub więcej");
+								npc.say("Poprosiła mnie o pomoc Anastazja, dobrze się składa. Ty jej pomożesz.");
 								raiser.setCurrentState(ConversationStates.ATTENDING);
 							}
 						} else {
-							npc.say("Twój stan społeczny jest zbyt niski aby podjąć te zadanie. Wróć gdy zdobędziesz 350 lvl.");
+							npc.say("Wesprzyj moją przyjaciółkę Eltefię w jej marzeniu.");
 							raiser.setCurrentState(ConversationStates.ATTENDING);
 						}
 					} else {
-						npc.say("Nim mógłbym ulepszyć Tobie złotą ciupagę, wykonaj zadanie u Władcy Smoków!");
+						npc.say("Musisz pierw udowodnić swe dobre zamiary. Pomóż mojemu pierwszemu znajomemu Gazdy Bartka rozwiązać jego problem.");
 						raiser.setCurrentState(ConversationStates.ATTENDING);
 					}
 				}
@@ -133,41 +157,50 @@ public class ZlotaCiupagaTrzyWasy extends AbstractQuest {
 	private void step_2() {
 		final SpeakerNPC npc = npcs.get("Hadrin");
 		final List<ChatAction> ciupagaactions = new LinkedList<ChatAction>();
-		ciupagaactions.add(new DropItemAction("pazur arktycznego smoka",1));
-		ciupagaactions.add(new DropItemAction("złota ciupaga z dwoma wąsami",1));
-		ciupagaactions.add(new DropItemAction("sztabka złota",200));
-		ciupagaactions.add(new DropItemAction("money",2000000));
-		ciupagaactions.add(new DropItemAction("polano",50));
-		ciupagaactions.add(new DropItemAction("pióro azazela",1));
+		ciupagaactions.add(new DropItemAction("pazur arktycznego smoka", 1));
+		ciupagaactions.add(new DropItemAction("pazur niebieskiego smoka", 1));
+		ciupagaactions.add(new DropItemAction("pazur czarnego smoka", 1));
+		ciupagaactions.add(new DropItemAction("złota ciupaga z dwoma wąsami", 1));
+		ciupagaactions.add(new DropItemAction("sztabka złota", 240));
+		ciupagaactions.add(new DropItemAction("money", 2500000));
+		ciupagaactions.add(new DropItemAction("polano", 70));
+		ciupagaactions.add(new DropItemAction("pióro azazela", 3));
 		ciupagaactions.add(new SetQuestAction(QUEST_SLOT, "forging;" + System.currentTimeMillis()));
 
 		npc.add(ConversationStates.ATTENDING, Arrays.asList("przedmioty", "przypomnij", "ciupaga"),
 				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"),
-								 new PlayerHasItemWithHimCondition("pazur arktycznego smoka",1),
-								 new PlayerHasItemWithHimCondition("złota ciupaga z dwoma wąsami",1),
-								 new PlayerHasItemWithHimCondition("sztabka złota",200),
-								 new PlayerHasItemWithHimCondition("money",2000000),
-								 new PlayerHasItemWithHimCondition("polano",50),
-								 new PlayerHasItemWithHimCondition("pióro azazela",1)),
+								 new PlayerHasItemWithHimCondition("pazur arktycznego smoka", 1),
+								 new PlayerHasItemWithHimCondition("pazur niebieskiego smoka", 1),
+								 new PlayerHasItemWithHimCondition("pazur czarnego smoka", 1),
+								 new PlayerHasItemWithHimCondition("złota ciupaga z dwoma wąsami", 1),
+								 new PlayerHasItemWithHimCondition("sztabka złota", 240),
+								 new PlayerHasItemWithHimCondition("money", 2500000),
+								 new PlayerHasItemWithHimCondition("polano", 70),
+								 new PlayerHasItemWithHimCondition("pióro azazela", 3)),
 				ConversationStates.IDLE, "Widzę, że masz wszystko o co cię prosiłem. Wróć za 24 godzin, a złota ciupaga z trzema wąsami będzie gotowa. Przypomnij mi mówiąc #'nagroda'.",
 				new MultipleActions(ciupagaactions));
 
 		npc.add(ConversationStates.ATTENDING, "przypomnij",
 				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"),
 								 new NotCondition(
-								 new AndCondition(new PlayerHasItemWithHimCondition("pazur arktycznego smoka",1),
-												  new PlayerHasItemWithHimCondition("złota ciupaga z dwoma wąsami",1),
-												  new PlayerHasItemWithHimCondition("sztabka złota",200),
-												  new PlayerHasItemWithHimCondition("money",2000000),
-												  new PlayerHasItemWithHimCondition("polano",50),
-												  new PlayerHasItemWithHimCondition("pióro azazela",1)))),
+								 new AndCondition(
+										 new PlayerHasItemWithHimCondition("pazur arktycznego smoka", 1),
+										 new PlayerHasItemWithHimCondition("pazur niebieskiego smoka", 1),
+										 new PlayerHasItemWithHimCondition("pazur czarnego smoka", 1),
+										 new PlayerHasItemWithHimCondition("złota ciupaga z dwoma wąsami", 1),
+										 new PlayerHasItemWithHimCondition("sztabka złota", 240),
+										 new PlayerHasItemWithHimCondition("money", 2500000),
+										 new PlayerHasItemWithHimCondition("polano", 70),
+										 new PlayerHasItemWithHimCondition("pióro azazela", 3)))),
 				ConversationStates.IDLE, "Do jej udoskonalenia potrzebuję:\n"
 								+"#'1 pazur arktycznego smoka'\n"
+								+"#'1 pazur niebieskiego smoka'\n"
+								+"#'1 pazur czarnego smoka'\n"
 								+"#'1 złota ciupaga z dwoma wąsami'\n"
-								+"#'200 sztabek złota'\n"
-								+"#'2000000 money'\n"
-								+"#'50 polan' oraz\n"
-								+"#'1 pióro azazela'\n"
+								+"#'240 sztabek złota'\n"
+								+"#'2500000 money'\n"
+								+"#'70 polan' oraz\n"
+								+"#'3 pióro azazela'\n"
 								+"Proszę przynieś mi to wszystko naraz. Jeżeli zapomnisz co masz przynieść to powiedz #'przypomnij'. Dziękuję!", null);
 	}
 
