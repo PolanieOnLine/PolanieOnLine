392bfc8a93f4e661d84a109aff56ac7290e45c79
KarajuSs <karajussgg@gmail.com>
2021-07-13 21:44:33 +0200

    Zaktualizowano wersję pliku

--- runpte.bat
+++ runpte.bat
@@ -1,5 +1,5 @@
 @echo off
 call ant compile_polanieonlinetools server_build
 
-set LOCALCLASSPATH=build\build_polanieonlinetools;build\build_server;build\build_server_maps;libs\marauroa.jar;libs\log4j.jar;libs\commons-lang.jar;libs\groovy.jar;libs\mysql-connector-java-5.1.5-bin.jar;libs\h2.jar;libs\swing-layout.jar;.
+set LOCALCLASSPATH=build\build_polanieonlinetools;build\build_server;build\build_server_maps;libs\marauroa.jar;libs\log4j.jar;libs\commons-lang.jar;libs\groovy.jar;libs\mysql-connector-java-8.0.13.jar;libs\h2.jar;libs\swing-layout.jar;.
 javaw -cp "%LOCALCLASSPATH%" games.stendhal.tools.npcparser.TestEnvDlg
--- wordlist.bat
+++ wordlist.bat
@@ -1,5 +1,5 @@
 @echo off
 call ant compile_polanieonlinetools server_build
 
-set LOCALCLASSPATH=build\build_polanieonlinetools;build\build_server;libs\marauroa.jar;libs\log4j.jar;libs\mysql-connector-java-5.1.jar;libs\h2.jar
+set LOCALCLASSPATH=build\build_polanieonlinetools;build\build_server;libs\marauroa.jar;libs\log4j.jar;libs\mysql-connector-java-8.0.13.jar;libs\h2.jar
 javaw -cp "%LOCALCLASSPATH%" games.stendhal.tools.npcparser.WordListUpdate
