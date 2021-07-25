1857aeea9e7e8da205a6e68ec8d98470e27e5b63
KarajuSs <karajussgg@gmail.com>
2021-07-25 21:19:19 +0200

    Update NameChange.java

--- src/games/stendhal/server/script/NameChange.java
+++ src/games/stendhal/server/script/NameChange.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import games.stendhal.server.core.scripting.ScriptImpl;
 import games.stendhal.server.entity.player.Player;
-import marauroa.common.Configuration;
 
 /**
  * Makes client display a fake player name by changing the title attribute. If
@@ -25,23 +24,8 @@ import marauroa.common.Configuration;
  * @author timothyb89
  */
 public class NameChange extends ScriptImpl {
-	private static final String CONFIG_KEY = "stendhal.scripts.namechange.enabled";
-
 	@Override
 	public void execute(final Player admin, final List<String> args) {
-		// check configuration
-		try {
-			if (!Configuration.getConfiguration().has(CONFIG_KEY)
-					|| !Boolean.parseBoolean(Configuration.getConfiguration().get(
-							CONFIG_KEY))) {
-				admin.sendPrivateText("Ten skrypt musi być włączony w pliku konfiguracyjnym serwera (zwykle server.ini) z kluczem "
-						+ CONFIG_KEY);
-				return;
-			}
-		} catch (final Exception e) {
-			admin.sendPrivateText(e.toString());
-			return;
-		}
 		if (args.size() < 1) {
 			admin.sendPrivateText("Użyj: /script NameChange.class {nowanazwa|remove}\nZmiania twoją nazwę na nową lub usuwa efekty zmiany nazwy.\nUwaga: Nie zalecane dla normalnych postaci. Używanie w przypadku przedmiotów lub małżonków może spowodować ich uszkodzenie lub inne nieprzewidziane efekty.");
 		} else {
