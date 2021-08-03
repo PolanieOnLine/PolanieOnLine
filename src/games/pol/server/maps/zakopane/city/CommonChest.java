31f0bd7a70355717f088010ae132dff3dda3db89
KarajuSs <karajussgg@gmail.com>
2021-08-03 22:47:45 +0200

    Update CommonChest.java

--- src/games/pol/server/maps/zakopane/city/CommonChest.java
+++ src/games/pol/server/maps/zakopane/city/CommonChest.java
@@ -11,12 +11,12 @@
  ***************************************************************************/
 package games.pol.server.maps.zakopane.city;
 
+import java.util.Map;
+
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.mapstuff.chest.Chest;
-
-import java.util.Map;
 
 /**
  * @author Legolas
@@ -35,7 +35,6 @@ public class CommonChest implements ZoneConfigurator {
 	}
 
 	private void buildZakopaneCityAreaChest(final StendhalRPZone zone) {
-
 		final Chest chest = new Chest();
 		chest.setPosition(110, 45);
 		chest.add(SingletonRepository.getEntityManager().getItem("mieczyk"));
