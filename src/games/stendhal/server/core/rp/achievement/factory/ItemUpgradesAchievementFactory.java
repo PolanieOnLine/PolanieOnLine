390dba997282e2fb9b1948f137c08335b4863aad
KarajuSs <karajussgg@gmail.com>
2022-01-27 23:44:44 +0100

    Dodano 3 nowe osiągnięcia

--- src/games/stendhal/server/core/rp/achievement/factory/ItemUpgradesAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/ItemUpgradesAchievementFactory.java
@@ -18,20 +18,31 @@ import java.util.List;
 import games.stendhal.server.core.rp.achievement.Achievement;
 import games.stendhal.server.core.rp.achievement.Category;
 import games.stendhal.server.core.rp.achievement.condition.ItemImprovedNumberOfCondition;
+import games.stendhal.server.core.rp.achievement.condition.ItemIsImprovedCondition;
 import games.stendhal.server.core.rp.achievement.condition.ItemIsMaxImprovedCondition;
 
 public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 	public static final String ID_SKETCH = "item.upgrade.sketch";
 	public static final String ID_INVEST = "item.upgrade.investment";
 	public static final String ID_PERUN = "item.upgrade.perun";
+	public static final String ID_DAGGERS = "item.upgrade.daggers";
 	public static final String ID_MITHRILRING = "item.upgrade.mithrilring";
 	public static final String ID_MITHRIL = "item.upgrade.mithrils";
+	public static final String ID_WANDS = "item.upgrade.wands";
+	public static final String ID_MAGICSET = "item.upgrade.magicset";
 
-	public static final String[] ITEM_PERUN = { "różdżka Peruna" };
-	public static final String[] ITEM_MITHRILRING = { "pierścień z mithrilu" };
 	public static final String[] ITEMS_MIHTIRL = {
 			"tarcza z mithrilu", "spodnie z mithrilu", "pas z mithrilu",
-			"hełm z mithrilu", "buty z mithrilu", "płaszcz z mithrilu" };
+			"hełm z mithrilu", "buty z mithrilu", "płaszcz z mithrilu"
+	};
+	public static final String[] ITEMS_WANDS = {
+			"różdżka", "trójząb Trzygłowa", "różdżka Strzyboga",
+			"różdżka Wołosa", "różdżka Swaroga", "różdżka Peruna"
+	};
+	public static final String[] ITEMS_MAGICSET = {
+			"magiczne rękawice płytowe", "magiczna zbroja płytowa",
+			"magiczne buty płytowe", "magiczne spodnie płytowe"
+	};
 
 	@Override
 	public Collection<Achievement> createAchievements() {
@@ -50,17 +61,32 @@ public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 		itemAchievements.add(createAchievement(
 				ID_PERUN, "Wspaniała Różdżka", "Ulepszył różdżkę Peruna do jej maksymalnego poziomu",
 				Achievement.MEDIUM_BASE_SCORE, true,
-				new ItemIsMaxImprovedCondition(ITEM_PERUN)));
+				new ItemIsMaxImprovedCondition("różdżka Peruna")));
+
+		itemAchievements.add(createAchievement(
+				ID_DAGGERS, "Potężne i Szybkie", "Ulepszył sztylecik z mithrilu oraz złotą klinge do ich maksymalnego poziomu",
+				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsMaxImprovedCondition("sztylecik z mithrilu", "złota klinga")));
 
 		itemAchievements.add(createAchievement(
 				ID_MITHRILRING, "Światłość", "Ulepszył pierścień z mithrilu do jej maksymalnego poziomu",
 				Achievement.MEDIUM_BASE_SCORE, true,
-				new ItemIsMaxImprovedCondition(ITEM_MITHRILRING)));
+				new ItemIsMaxImprovedCondition("pierścień z mithrilu")));
 
 		itemAchievements.add(createAchievement(
 				ID_MITHRIL, "Doskonałe Uzbrojenie", "Ulepszył tarczę, spodnie, pas, hełm, buty oraz płaszcz z mithrilu do maksymalnego poziomu",
 				Achievement.HARD_BASE_SCORE, true,
 				new ItemIsMaxImprovedCondition(ITEMS_MIHTIRL)));
+
+		itemAchievements.add(createAchievement(
+				ID_WANDS, "Zabawa w Czarodzieja", "Ulepszył wszystkie różdżki co najmniej raz",
+				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsImprovedCondition(ITEMS_WANDS)));
+
+		itemAchievements.add(createAchievement(
+				ID_MAGICSET, "Jeszcze Bardziej Magicznie", "Ulepszył cały magiczny zestaw wyposażenia co najmniej raz",
+				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsImprovedCondition(ITEMS_MAGICSET)));
 
 		return itemAchievements;
 	}
