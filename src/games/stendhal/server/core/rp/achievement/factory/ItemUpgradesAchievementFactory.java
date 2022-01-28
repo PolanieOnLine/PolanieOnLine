30bf33aea4a3d75dd952cafa291e348ecbd68b4f
KarajuSs <karajussgg@gmail.com>
2022-01-28 15:40:08 +0100

    Dodano nowe osiągnięcia

--- src/games/stendhal/server/core/rp/achievement/factory/ItemUpgradesAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/ItemUpgradesAchievementFactory.java
@@ -28,8 +28,12 @@ public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 	public static final String ID_DAGGERS = "item.upgrade.daggers";
 	public static final String ID_MITHRILRING = "item.upgrade.mithrilring";
 	public static final String ID_MITHRIL = "item.upgrade.mithrils";
+	public static final String ID_BLACK = "item.upgrade.blackset";
 	public static final String ID_WANDS = "item.upgrade.wands";
 	public static final String ID_MAGICSET = "item.upgrade.magicset";
+	public static final String ID_STONE = "item.upgrade.stoneshield";
+	public static final String ID_AXES = "item.upgrade.axes";
+	public static final String ID_GORAL = "item.upgrade.goral";
 
 	public static final String[] ITEMS_MIHTIRL = {
 			"tarcza z mithrilu", "spodnie z mithrilu", "pas z mithrilu",
@@ -42,6 +46,13 @@ public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 	public static final String[] ITEMS_MAGICSET = {
 			"magiczne rękawice płytowe", "magiczna zbroja płytowa",
 			"magiczne buty płytowe", "magiczne spodnie płytowe"
+	};
+	public static final String[] ITEMS_BLACKSET = {
+			"czarna zbroja", "czarne spodnie",
+			"czarna tarcza", "czarne buty"
+	};
+	public static final String[] ITEMS_AXES = {
+			"czarna kosa", "złota kosa", "kosa z mithrilu"
 	};
 
 	@Override
@@ -65,7 +76,7 @@ public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 
 		itemAchievements.add(createAchievement(
 				ID_DAGGERS, "Potężne i Szybkie", "Ulepszył sztylecik z mithrilu oraz złotą klinge do ich maksymalnego poziomu",
-				Achievement.MEDIUM_BASE_SCORE, true,
+				Achievement.EASY_BASE_SCORE, true,
 				new ItemIsMaxImprovedCondition("sztylecik z mithrilu", "złota klinga")));
 
 		itemAchievements.add(createAchievement(
@@ -79,14 +90,34 @@ public class ItemUpgradesAchievementFactory extends AbstractAchievementFactory {
 				new ItemIsMaxImprovedCondition(ITEMS_MIHTIRL)));
 
 		itemAchievements.add(createAchievement(
-				ID_WANDS, "Zabawa w Czarodzieja", "Ulepszył wszystkie różdżki co najmniej raz",
+				ID_BLACK, "Opanowany przez Mrok", "Ulepszył czarną tarczę, spodnie, zbroję oraz buty do maksymalnego poziomu",
 				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsMaxImprovedCondition(ITEMS_BLACKSET)));
+
+		itemAchievements.add(createAchievement(
+				ID_WANDS, "Zabawa w Czarodzieja", "Ulepszył wszystkie różdżki co najmniej raz",
+				Achievement.EASY_BASE_SCORE, true,
 				new ItemIsImprovedCondition(ITEMS_WANDS)));
 
 		itemAchievements.add(createAchievement(
 				ID_MAGICSET, "Jeszcze Bardziej Magicznie", "Ulepszył cały magiczny zestaw wyposażenia co najmniej raz",
 				Achievement.MEDIUM_BASE_SCORE, true,
 				new ItemIsImprovedCondition(ITEMS_MAGICSET)));
+
+		itemAchievements.add(createAchievement(
+				ID_STONE, "Kamienna Broń", "Ulepszył kamienną tarczę na co najmniej drugi poziom",
+				Achievement.EASY_BASE_SCORE, true,
+				new ItemIsImprovedCondition(2, "kamienna tarcza")));
+
+		itemAchievements.add(createAchievement(
+				ID_AXES, "Uzbrojenie Kosiarza", "Ulepszył czarną, złotą oraz z mithrilu kosę do maksymalnego poziomu",
+				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsMaxImprovedCondition(ITEMS_AXES)));
+
+		itemAchievements.add(createAchievement(
+				ID_GORAL, "Dusza Góralska", "Ulepszył złotą ciupagę z trzema wąsami do maksymalnego poziomu",
+				Achievement.MEDIUM_BASE_SCORE, true,
+				new ItemIsMaxImprovedCondition("złota ciupaga z trzema wąsami")));
 
 		return itemAchievements;
 	}
