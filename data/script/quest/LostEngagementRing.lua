84f4a2acf84532c7e32fd46f049fd5bdff14b16b
KarajuSs <karajussgg@gmail.com>
2022-03-11 17:23:56 +0100

    Dodano nowy przedmiot oraz możliwość jego wypożyczenia

--- data/script/quest/LostEngagementRing.lua
+++ data/script/quest/LostEngagementRing.lua
@@ -26,6 +26,7 @@ local quest_slot = "lost_engagement_ring"
 
 local ari
 
+local minLevel = 50
 local karmaAcceptReward = 15
 local karmaCompleteReward = 50
 
@@ -50,6 +51,7 @@ local hasRingCondition = conditions:create("PlayerHasInfostringItemWithHimCondit
 local hasKeyringCondition = conditions:create(function(player, sentence, npc)
 	return player:hasFeature("keyring")
 end)
+local minLevelCondition = conditions:create("LevelGreaterThanCondition", {minLevel - 1})
 
 --[[ FIXME: conditions:create is struggling with constructors that take varargs
 local visitedAthorCondition = conditions:create("PlayerVisitedZonesCondition", {
@@ -65,31 +67,49 @@ local canStartCondition = conditions:andC({
 	conditions:notC(questCompletedCondition),
 	hasKeyringCondition,
 	visitedAthorCondition,
+	minLevelCondition,
 })
+
+local chooseRingLocation = function()
+	return ring_locations[random:randUniform(1, 4)]
+end
 
 local setQuestAction = function(player, sentence, npc)
 	player:addKarma(karmaAcceptReward)
 
 	-- choose random location
-	local selected = random:randUniform(1, 4)
-
-	player:setQuest(quest_slot, 0, ring_locations[selected][1])
-	player:setQuest(quest_slot, 1, ring_locations[selected][2])
+	local selected = chooseRingLocation()
+	player:setQuest(quest_slot, 0, selected[1])
+	player:setQuest(quest_slot, 1, selected[2])
 end
 
 local resetQuestAction = function(player, sentence, npc)
 	if player:hasQuest(quest_slot) then
 		local slots = player:getQuest(quest_slot):split(";")
-		if slots[1] == "have_ring" then
-			-- FIXME: should have a failsafe here in case slot string is malformatted
-			player:setQuest(quest_slot, slots[2] .. ";" .. slots[3])
+		local x = slots[1]
+		local y = slots[2]
+
+		if not string.isNumber(x) then
+			x = slots[2]
+			y = slots[3]
 		end
+
+		if not string.isNumber(x) or not string.isNumber(y) then
+			logger:warn(quest_slot .. " quest: malformatted quest slot, setting"
+				.. " new ring coordinates")
+
+			local selected = chooseRingLocation()
+			x = selected[1]
+			y = selected[2]
+		end
+
+		player:setQuest(quest_slot, x .. ";" .. y)
 	end
 end
 
 local rewardAction = function(player, sentence, npc)
 	player:addKarma(karmaCompleteReward)
-	player:setFeature("keyring_ext", true)
+	player:setFeature("keyring", "6 3")
 
 	local slots = player:getQuest(quest_slot):split(";")
 	if #slots > 2 then
@@ -116,10 +136,11 @@ local prepareRequestStep = function()
 		ConversationPhrases.GREETING_MESSAGES,
 		conditions:orC({
 			{
-				conditions:create("QuestNotStartedCondition", {quest_slot}),
+				questNotStartedCondition,
 				conditions:orC({
 					conditions:notC(hasKeyringCondition),
 					conditions:notC(visitedAthorCondition),
+					conditions:notC(minLevelCondition),
 				}),
 			},
 			questCompletedCondition,
@@ -127,13 +148,25 @@ local prepareRequestStep = function()
 		ConversationStates.ATTENDING,
 		"Cześć!",
 		nil)
+		
+	-- player doesn't meet minimum level requirement
+	ari:add(
+		ConversationStates.ATTENDING,
+		ConversationPhrases.QUEST_MESSAGES,
+		{
+			questNotStartedCondition,
+			conditions:notC(minLevelCondition),
+		},
+		ConversationStates.ATTENDING,
+		"Myślę, że nie masz doświadczenia, aby mi pomóc. Wróć, gdy staniesz się silniejszy.",
+		nil)
 
 	-- player does not have keyring
 	ari:add(
 		ConversationStates.ATTENDING,
 		ConversationPhrases.QUEST_MESSAGES,
 		{
-			conditions:create("QuestNotStartedCondition", {quest_slot}),
+			questNotStartedCondition,
 			conditions:notC(hasKeyringCondition),
 		},
 		ConversationStates.ATTENDING,
@@ -146,7 +179,7 @@ local prepareRequestStep = function()
 		ConversationStates.ATTENDING,
 		ConversationPhrases.QUEST_MESSAGES,
 		{
-			conditions:create("QuestNotStartedCondition", {quest_slot}),
+			questNotStartedCondition,
 			conditions:notC(visitedAthorCondition),
 		},
 		ConversationStates.ATTENDING,
@@ -222,7 +255,8 @@ local prepareBringStep = function()
 		nil,
 		ConversationStates.IDLE,
 		"Mogłem go upuścić podczas spaceru po plaży. Jeśli tak jest,"
-			.. " możesz potrzebować czegoś do kopania w piasku.",
+			.. " możesz potrzebować czegoś do kopania w piasku lub coś, co może wykryć metal."
+			.. " Może lombard w Deniran ma narzędzie, którego możesz użyć.",
 		nil)
 
 	ari:add(
@@ -256,7 +290,7 @@ local prepareBringStep = function()
 
 	ari:add(
 		ConversationStates.QUESTION_1,
-		"lost",
+		{ "lost", "zgubiony", "stracony" },
 		conditions:notC(hasRingCondition),
 		ConversationStates.IDLE,
 		"Zgubiłeś mój zgubiony pierścionek zaręczynowy? To trochę ironiczne."
@@ -266,7 +300,7 @@ local prepareBringStep = function()
 
 	ari:add(
 		ConversationStates.QUESTION_1,
-		"lost",
+		{ "lost", "zgubiony", "stracony" },
 		hasRingCondition,
 		ConversationStates.IDLE,
 		"Zgubiłeś mój zgubiony pierścionek zaręczynowy? To trochę ironiczne."
@@ -275,8 +309,364 @@ local prepareBringStep = function()
 end
 
 
-quests:create(quest_slot, "Zgubiony Pierścionek Zaręczynowy"):register(function()
+-- set up metal detector lender
+
+-- we use a quest slot so player cannot borrow multiple metal detectors
+local lender_slot = "sawyer_metal_detector"
+local loanActive = conditions:create("QuestActiveCondition", {lender_slot})
+local lend_phrases = {"loan", "lend", "metal detector", "borrow", "pożyczyć", "wypożyczyć", "wykrywacz"}
+--local return_phrases = {"return", "zwrócić, "zwrot"}
+
+-- items that Sawyer will take in exchange for his metal detector
+local collateral = {
+	-- sword
+	"czarny miecz",
+	"sztylet chaosu",
+	"ognisty miecz demonów",
+	"złota klinga",
+	"złota klinga orków",
+	"piekielny sztylet",
+	--"miecz nieśmiertelnych",
+	"miecz cesarski",
+	"nihonto",
+	"miecz orków",
+	"sztylet królewski",
+	"sztylet dusz",
+	"miecz xenocyjski",
+
+	-- axe
+	"czarna halabarda",
+	"czarna kosa",
+	"topór chaosu",
+	"topór Durina",
+	--"magiczny topór obosieczny",
+
+	-- club
+	"lodowy młot bojowy",
+	"młot wulkanów",
+
+	-- ranged
+	"łuk z mithrilu",
+
+	-- armor
+	"czarna zbroja",
+	"lodowa zbroja",
+	--"magiczna zbroja płytowa",
+	--"zbroja mainiocyjska",
+	"zbroja z mithrilu",
+	"zbroja monarchistyczna",
+
+	-- boots
+	--"czarne buty",
+	--"magiczne buty płytowe",
+	"buty z mithrilu",
+	"buty monarchistyczne",
+
+	-- cloak
+	--"czarny płaszcz",
+	"płaszcz licha",
+	--"magiczny płaszcz",
+	"płaszcz z mithrilu",
+	"płaszcz monarchistyczny",
+	"płaszcz wampirzy",
+
+	-- helmet
+	"czarny hełm",
+	"hełm libertyński",
+	"magiczny hełm kolczy",
+	"hełm z mithrilu",
+	"hełm monarchistyczny",
+
+	-- legs
+	--"czarne spodnie",
+	--"spodnie nabijane klejnotami",
+	--"magiczne spodnie płytowe",
+	"spodnie z mithrilu",
+	"spodnie monarchistyczne",
+
+	-- ring
+	"pierścień szmaragdowy",
+	"wzmocniony pierścień imperialny",
+	"pierścień imperialny",
+	"pierścień skorupy żółwia",
+
+	-- shield
+	"czarna tarcza",
+	"lodowa tarcza",
+	--"magiczna tarcza płytowa",
+	"tarcza z mithrilu",
+	"tarcza monarchistyczna",
+	--"tarcza xenocyjska",
+}
+
+local loanMetalDetector = function(player, lender, detector, offer, bound_to, info_s)
+	-- player walked away before receiving metal detector
+	if lender:getAttending() == nil then
+		lender:say("Gdzie poszedłeś? No cóż, chyba po prostu odłożę to z powrotem na swoje miejsce.")
+		sawyersShelf:returnMetalDetector(detector)
+		lender:setCurrentState(ConversationStates.IDLE)
+		return
+	end
+
+	-- problem with metal detector item
+	if detector == nil then
+		lender:say("O o! Wygląda na to, że mój wykrywacz metalu jest zepsuty. Przepraszam kolego. Może mógłbyś"
+			.. " skontaktować się z #/support i poprosić kogoś o naprawienie tego dla mnie.")
+		lender:setCurrentState(ConversationStates.ATTENDING)
+		return
+	end
+
+	local traded = player:getFirstEquipped(offer)
+
+	if traded == nil then
+		lender:say("Ha! Chcesz mnie oszukać?")
+		lender:setCurrentState(ConversationStates.ATTENDING)
+		return
+	end
+
+	-- handle items with infostring & bound items
+	local bound_to = traded:getBoundTo() or ""
+	local info_s = traded:getInfoString() or ""
+
+	local slot_state = offer .. ";" .. bound_to .. ";" .. info_s
+
+	detector:setInfoString("Sawyer;" .. slot_state)
+	detector:setBoundTo(player:getName())
+	detector:setUndroppableOnDeath(true)
+
+	player:setQuest(lender_slot, slot_state)
+	player:drop(traded)
+	player:equipOrPutOnGround(detector)
+
+	lender:say("Ok, proszę bardzo. Mój wykrywacz metali za twój " .. offer
+		.. ". Uważaj na to. Jeśli się zgubi, nie będziesz mógł go"
+		.. " #zwrócić, a ja zachowam twój przedmiot " .. offer .. ". W czymś"
+		.. " jeszcze mógłbym Ci pomóc?")
+	lender:setCurrentState(ConversationStates.ATTENDING)
+end
+
+local handleLendRequest = function(player, sentence, lender)
+	local offer = sentence:getTrimmedText()
+
+	-- FIXME: using table.contains function breaks ChatAction.fire method created with actions:create
+
+	-- player ends conversation
+	--if not table.contains(arrays:toTable(ConversationPhrases.GOODBYE_MESSAGES), offer:lower()) then
+	for _, msg in ipairs(arrays:toTable(ConversationPhrases.GOODBYE_MESSAGES)) do
+		if offer:lower() == msg then
+			lender:getEntity():endConversation()
+			return
+		end
+	end
+
+	-- player decides not to trade
+	-- FIXME: arrays:toTable does not parse ConversationPhrases.NO_MESSAGES correctly
+	for _, msg in ipairs({"no", "nope", "nothing", "none", "nie"}) do
+		if offer:lower() == msg then
+			lender:say("W porządku. W czym jeszcze mogę Ci pomóc?")
+			return
+		end
+	end
+
+	-- Sawyer will not accept offered item
+	--if not table.contains(collateral, offer) then
+	local accepted = false
+	for _, acceptable in ipairs(collateral) do
+		if offer == acceptable then
+			accepted = true
+			break
+		end
+	end
+
+	if not accepted then
+		lender:say("Hmmm, nie jestem tym zainteresowany. Co jeszcze masz?")
+		lender:setCurrentState(ConversationStates.QUESTION_1)
+		return;
+	end
+
+	-- player isn't carrying the item
+	if not player:isEquipped(offer) then
+		lender:say("Nie masz nawet jednego z nich. Daj spokój, co masz?")
+		lender:setCurrentState(ConversationStates.QUESTION_1)
+		return;
+	end
+
+	local detector = sawyersShelf:getMetalDetector()
+
+	-- metal detector from shelf was already loaned out
+	if detector == nil then
+		lender:say("Hmmmm. I guess I forgot that I already loaned it out...")
+		lender:say("!me is thinking.")
+		lender:setCurrentState(ConversationStates.BUSY)
+
+		game:runAfter(10, function()
+			lender:say("Oh! That's right, I keep a spare just in case.")
+			detector = sawyersShelf:getSpareMetalDetector()
+
+			game:runAfter(5, function()
+				loanMetalDetector(player, lender, detector, offer,
+					bound_to, info_s)
+			end)
+		end)
+
+		return
+	end
+
+	-- metal detector from shelf
+	loanMetalDetector(player, lender, detector, offer, bound_to, info_s)
+end
+
+local handleReturnRequest = function(player, sentence, lender)
+	if not player:isEquipped("wykrywacz metali") then
+		lender:say("Nie masz nawet wykrywacza metalu.")
+		return
+	end
+
+	local detector = player:getFirstEquipped("wykrywacz metali")
+	local detector_info = (detector:getInfoString() or ""):split(";")
+
+	-- Sawyer doesn't recognize the metal detector
+	if #detector_info == 0 or detector_info[1] ~= "Sawyer" then
+		lender:say("To nie jest moje. Zabierz to z powrotem. Chcę MÓJ wykrywacz metali.")
+		return
+	end
+
+	local item_name = detector_info[2]
+	local bound_to = detector_info[3]
+	local info_s = detector_info[4]
+
+	local item = entities:getItem(item_name)
+
+	-- problem with item
+	if item == nil then
+		lender:say("O o! Wygląda na to, że zepsułem twój przedmiot " .. item_name .. "."
+			.. " Przepraszam kolego. Może mógłbyś skontaktować się z #/support i poprosić kogoś"
+			.. " o naprawienie tego dla mnie.")
+		return
+	end
+
+	if bound_to ~= "" then
+		item:setBoundTo(bound_to)
+	end
+
+	if info_s ~= "" then
+		item:setInfoString(info_s)
+	end
+
+	player:drop(detector)
+	player:equipOrPutOnGround(item)
+	player:setQuest(lender_slot, nil)
+
+	sawyersShelf:returnMetalDetector(detector)
+	lender:say("Dobra. Oto twój przedmiot " .. item_name .. ". Wygląda jak nowy."
+		.. " Czy mogę jeszcze w czymś pomóc?")
+end
+
+local prepareMetalDetectorLender = function()
+	local lender = entities:getNPC("Sawyer")
+	if lender == nil then
+		logger:error("Cannot set up metal detector lender Sawyer for Lost Engagement Ring quest")
+		return
+	end
+
+	-- hint to players that they can borrow metal detector
+	lender:addOffer("Mam też parę rzeczy, których nie mam nic przeciwko, aby #wypożyczyć.")
+
+	lender:add(
+		ConversationStates.ATTENDING,
+		lend_phrases,
+		conditions:notC(loanActive),
+		ConversationStates.QUESTION_1,
+		"Więc chcesz pożyczyć mój wykrywacz metalu? Cóż, nie pożyczam rzeczy"
+			.. " bez jakiejś formy zabezpieczenia. Co chciałbyś zostawić?",
+		nil)
+
+	lender:add(
+		ConversationStates.ATTENDING,
+		lend_phrases,
+		loanActive,
+		ConversationStates.ATTENDING,
+		"Już pożyczyłem ci mój wykrywacz metalu. Możesz go #zwrócić, kiedy tylko chcesz.",
+		nil)
+
+	lender:add(
+		ConversationStates.QUESTION_1,
+		"",
+		conditions:notC(loanActive),
+		ConversationStates.ATTENDING,
+		nil,
+		handleLendRequest)
+
+	lender:add(
+		ConversationStates.ATTENDING,
+		"return",
+		conditions:notC(loanActive),
+		ConversationStates.ATTENDING,
+		"Co zwrócić? Niczego ci nie pożyczałem.",
+		nil)
+
+	lender:add(
+		ConversationStates.ATTENDING,
+		"return",
+		loanActive,
+		ConversationStates.QUESTION_1,
+		"Chcesz zwrócić mój wykrywacz metalu?",
+		nil)
+
+	lender:add(
+		ConversationStates.QUESTION_1,
+		ConversationPhrases.NO_MESSAGES,
+		loanActive,
+		ConversationStates.ATTENDING,
+		"Dobra. W czym jeszcze mogę Ci pomóc?",
+		nil)
+
+	lender:add(
+		ConversationStates.QUESTION_1,
+		ConversationPhrases.YES_MESSAGES,
+		loanActive,
+		ConversationStates.ATTENDING,
+		nil,
+		handleReturnRequest)
+end
+
+
+local quest = quests:create(quest_slot, "Zgubiony Pierścionek Zaręczynowy",
+	"Para, która ma wziąć ślub w mieście Fado, potrzebuje pomocy.")
+quest:setMinLevel(minLevel)
+quest:setRegion(Region.FADO_CITY)
+quest:setNPCName("Ari")
+
+quest:setHistoryFunction(function(player)
+	local history = {}
+
+	local state = player:getQuest(quest_slot, 0)
+	if state == "done" then
+		table.insert(history, "Znalazłem pierścionek Ariego. Teraz jest gotowy do małżeństwa z Emmą.")
+	elseif state == "found_ring" then
+		if player:isEquippedWithInfostring("pierścień zaręczynowy", ring_infostring) then
+			table.insert(history, "Znalazłem pierścień Ariego i powinienem mu go przynieść.")
+		else
+			table.insert(history, "Znalazłem pierścionek Ariego, ale chyba go zgubiłem."
+				.. " Powinienem zgłosić Ariemu, że <em>zgubiłem</em> go.")
+		end
+	else
+		table.insert(history, "Ari stracił pierścionek zaręczynowy podczas wakacji na wyspie Athor.")
+		if player:isEquipped("wykrywacz metali") then
+			table.insert(history, "Może uda mi się go znaleźć tym wykrywaczem metalu.")
+		elseif player:isEquipped("łopata") then
+			table.insert(history, "Może uda mi się go znaleźć tą łopatą.")
+		else
+			table.insert(history, "Potrzebuję narzędzia, żeby to znaleźć.")
+		end
+	end
+
+	return history
+end)
+
+quest:register(function()
 	prepareNPC()
 	prepareRequestStep()
 	prepareBringStep()
+	prepareMetalDetectorLender()
 end)
--- data/script/region/deniran/city/interior/pawn_shop/ItemsOnShelf.lua
+++ data/script/region/deniran/city/interior/pawn_shop/ItemsOnShelf.lua
@@ -15,12 +15,82 @@
 
 local zone_name = "int_deniran_pawn_shop"
 
-if game:setZone(zone_name) then
-	local goldenBladeGrower = luajava.newInstance("games.stendhal.server.entity.mapstuff.spawner.PassiveEntityRespawnPoint", "złota klinga", 288000)
-	goldenBladeGrower:setPosition(19, 3)
+-- make this global so LostEngagementRing can access
+sawyersShelf = {
+	put = function(self, item, x, y)
+		item:setBoundTo("Sawyer")
+		item:setPosition(x, y)
 
-	game:add(goldenBladeGrower)
-	goldenBladeGrower:setToFullGrowth()
+		local zone = game:getZone(zone_name)
+		if zone ~= nil then
+			zone:add(item, false)
+		else
+			logger:error("could not get zone " .. zone_name
+				.. " to place item " .. item:getName())
+		end
+	end,
+
+	get = function(self, x, y, pickup)
+		pickup = pickup ~= false -- default to true
+
+		local zone = game:getZone(zone_name)
+		if zone ~= nil then
+			local item = zone:getEntityAt(x, y)
+			if pickup and item ~= nil then
+				zone:remove(item)
+			end
+
+			return item
+		end
+	end,
+
+	getMetalDetector = function(self)
+		return self:get(19, 5)
+	end,
+
+	-- in case metal detector is already loaned out
+	getSpareMetalDetector = function(self)
+		return entities:getItem("wykrywacz metali")
+	end,
+
+	returnMetalDetector = function(self, detector)
+		-- don't add multiple metal detectors
+		if self:get(19, 5, false) == nil then
+			if detector == nil then
+				detector = entities:getItem("wykrywacz metali")
+				if detector == nil then
+					logger:error("Could not create metal detector for Deniran pawn shop shelf")
+					return
+				end
+			end
+
+			self:put(detector, 19, 5)
+		end
+	end,
+}
+
+if game:setZone(zone_name) then
+	local blade = entities:getItem("złota klinga")
+	if blade ~= nil then
+		sawyersShelf:put(blade, 19, 3)
+	else
+		logger:error("Could not create golden blade for Deniran pawn shop shelf")
+	end
+
+	sawyersShelf:returnMetalDetector()
+
+	-- add a note with info about using metal detector
+	local note = entities:create({
+		type = "Sign",
+		pos = {17, 6},
+		text = "Uwagi dotyczące korzystania z wykrywacza metali:"
+			.. "\n- Naciśnij przycisk \"Skanuj\", aby go włączyć."
+			.. "\n- Urządzenie wyda sygnał dźwiękowy, jeśli znajdziesz się w pobliżu."
+			.. "\n- Im bliżej jesteś, tym szybciej emituje sygnał dźwiękowy.",
+		class = "paper",
+	})
+
+	game:add(note)
 else
 	logger:error("could not set zone: " .. zone_name)
 end
