e117f7306d9a55bf14ce6d6c8507b07b38a2eab4
KarajuSs <karajussgg@gmail.com>
2022-11-24 02:53:10 +0100

    Zmieniono numer wersji

--- src/games/stendhal/server/core/engine/GenerateINI.java
+++ src/games/stendhal/server/core/engine/GenerateINI.java
@@ -271,7 +271,7 @@ public class GenerateINI {
 		out.println();
 		out.println("server_typeGame=" + gameName);
 		out.println("server_name=" + gameName + " Marauroa server");
-		out.println("server_version=1.32.2");
+		out.println("server_version=1.32.3");
 		out.println("server_contact=https://polanieonline.eu/kontakt-gmgags.html");
 		out.println();
 		out.println("# Extensions configured on the server. Enable at will.");
