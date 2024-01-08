ee858decd82f85e701c3a34122eb34354dec005f
KarajuSs <karajussgg@gmail.com>
2024-01-09 00:45:04 +0100

    Dodano możliwość spawnowania alternatywnych stworzeń w krainie

--- src/games/stendhal/server/maps/AlternativeCreatures.java
+++ src/games/stendhal/server/maps/AlternativeCreatures.java
@@ -14,6 +14,7 @@ package games.stendhal.server.maps;
 import java.util.Map;
 
 import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.creature.Creature;
 import games.stendhal.server.entity.mapstuff.spawner.CreatureRespawnPoint;
@@ -22,29 +23,51 @@ public class AlternativeCreatures implements ZoneConfigurator {
 	@Override
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
 		double factor = 0.1;
+		boolean spawnPoint = true;
 		if (attributes.containsKey("factor")) {
 			factor = Double.parseDouble(attributes.get("factor"));
 		}
+		if (isCreatureValid(attributes)) {
+			spawnPoint = false;
+			final Creature monster = SingletonRepository.getEntityManager().getCreature(attributes.get("creature"));
+			int x = Integer.parseInt(attributes.get("spawnX"));
+			int y = Integer.parseInt(attributes.get("spawnY"));
 
-		for (CreatureRespawnPoint spawner : zone.getRespawnPointList()) {
-			Creature c = spawner.getPrototypeCreature();
-			// Rare & abnormal & immortal creatures should not count.
-			if (c.isAbnormal()) {
-				continue;
-			}
-			c.setName("alt " + c.getName());
-			c.setAtk(configureStats(c.getAtk(), factor));
-			c.setDef(configureStats(c.getDef(), factor));
-			c.setBaseHP(configureStats(c.getBaseHP(), factor));
-			c.setHP(configureStats(c.getHP(), factor));
-			c.setXP(configureStats(c.getXP(), factor));
-			c.setLevel(configureStats(c.getLevel(), factor));
+			creatureAttributes(monster, factor);
 
-			c.addDropItem("ciemnolit", 0.5, 1);
+			final CreatureRespawnPoint spawnMonster = new CreatureRespawnPoint(zone, x, y, monster, 1);
+			zone.add(spawnMonster);
 		}
+
+		if (spawnPoint) {
+			for (CreatureRespawnPoint spawner : zone.getRespawnPointList()) {
+				Creature c = spawner.getPrototypeCreature();
+				// Rare & abnormal & immortal creatures should not count.
+				if (c.isAbnormal()) {
+					continue;
+				}
+				creatureAttributes(c, factor);
+			}
+		}
+	}
+	
+	private void creatureAttributes(Creature c, double factor) {
+		c.setName("alt " + c.getName());
+		c.setAtk(configureStats(c.getAtk(), factor));
+		c.setDef(configureStats(c.getDef(), factor));
+		c.setBaseHP(configureStats(c.getBaseHP(), factor));
+		c.setHP(configureStats(c.getHP(), factor));
+		c.setXP(configureStats(c.getXP(), factor));
+		c.setLevel(configureStats(c.getLevel(), factor));
+
+		c.addDropItem("ciemnolit", 0.2, 1);
 	}
 
 	private int configureStats(int value, double factor) {
 		return (int) (value * (1 + factor));
+	}
+
+	private boolean isCreatureValid(final Map<String, String> attr) {
+		return attr.containsKey("creature") && attr.containsKey("spawnX") && attr.containsKey("spawnY");
 	}
 }
