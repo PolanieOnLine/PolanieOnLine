027eca899148d2c7073b02969dda4e9610d236b5
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-05-02 09:53:00 +0200

    Poprawiono proporcje surowców wymaganych do ulepszenia uzbrojenia

--- src/games/stendhal/server/maps/tarnow/blacksmith/forge/ForgeItems.java
+++ src/games/stendhal/server/maps/tarnow/blacksmith/forge/ForgeItems.java
@@ -41,37 +41,37 @@ public class ForgeItems {
 
 	private static HashMap<String, Map<String, Integer>> getItemResources() {
 		final Map<String, Integer> productionHelmet = new TreeMap<String, Integer>();
-		productionHelmet.put("klejnot ciemnolitu", 3);
+		productionHelmet.put("klejnot ciemnolitu", 4);
 		productionHelmet.put("sztabka platyny", 9);
-		productionHelmet.put("bryłka mithrilu", 12);
+		productionHelmet.put("bryłka mithrilu", 16);
 		productionHelmet.put("hełm z mithrilu", 1);
 
 		final Map<String, Integer> productionArmor = new TreeMap<String, Integer>();
-		productionArmor.put("klejnot ciemnolitu", 5);
-		productionArmor.put("sztabka platyny", 7);
-		productionArmor.put("bryłka mithrilu", 14);
+		productionArmor.put("klejnot ciemnolitu", 9);
+		productionArmor.put("sztabka platyny", 10);
+		productionArmor.put("bryłka mithrilu", 19);
 		productionArmor.put("zbroja z mithrilu", 1);
 
 		final Map<String, Integer> productionLegs = new TreeMap<String, Integer>();
 		productionLegs.put("klejnot ciemnolitu", 3);
-		productionLegs.put("sztabka platyny", 10);
+		productionLegs.put("sztabka platyny", 7);
 		productionLegs.put("bryłka mithrilu", 15);
 		productionLegs.put("spodnie z mithrilu", 1);
 
 		final Map<String, Integer> productionBoots = new TreeMap<String, Integer>();
 		productionBoots.put("klejnot ciemnolitu", 2);
-		productionBoots.put("sztabka platyny", 5);
+		productionBoots.put("sztabka platyny", 8);
 		productionBoots.put("bryłka mithrilu", 9);
 		productionBoots.put("buty z mithrilu", 1);
 
 		final Map<String, Integer> productionShield = new TreeMap<String, Integer>();
-		productionShield.put("klejnot ciemnolitu", 7);
+		productionShield.put("klejnot ciemnolitu", 5);
 		productionShield.put("sztabka platyny", 14);
-		productionShield.put("bryłka mithrilu", 22);
+		productionShield.put("bryłka mithrilu", 24);
 		productionShield.put("tarcza z mithrilu", 1);
 
 		final Map<String, Integer> productionCloak = new TreeMap<String, Integer>();
-		productionCloak.put("klejnot ciemnolitu", 6);
+		productionCloak.put("klejnot ciemnolitu", 7);
 		productionCloak.put("sztabka platyny", 12);
 		productionCloak.put("bryłka mithrilu", 17);
 		productionCloak.put("płaszcz z mithrilu", 1);
