3b31c837c9e5a9d065a09f82536930d5f09b85d3
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-05-14 15:01:15 +0200

    Dodano opisy oraz naprawiono kopię konstruktora dla NewArms

--- src/games/stendhal/server/entity/item/NewArms.java
+++ src/games/stendhal/server/entity/item/NewArms.java
@@ -3,12 +3,30 @@ package games.stendhal.server.entity.item;
 import java.util.Map;
 
 public class NewArms extends StatusResistantItem {
-	public NewArms(StatusResistantItem item) {
-		super(item);
+	/**
+	 * Default constructor.
+	 *
+	 * @param name
+	 * 		Item's name
+	 * @param clazz
+	 * 		Item's class or type
+	 * @param subclass
+	 * 		Item's subclass
+	 * @param attributes
+	 * 		Attributes available to this item
+	 */
+	public NewArms(final String name, final String clazz, final String subclass, final Map<String, String> attributes) {
+		super(name, clazz, subclass, attributes);
 	}
 
-	public NewArms(String name, String clazz, String subclass, Map<String, String> attributes) {
-		super(name, clazz, subclass, attributes);
+	/**
+	 * Copy constructor.
+	 *
+	 * @param item
+	 * 		Item to copy
+	 */
+	public NewArms(final NewArms item) {
+		super(item);
 	}
 
 	@Override
