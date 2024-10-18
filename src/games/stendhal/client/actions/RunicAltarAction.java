6faa44c5ad026979bef1584d449f679b672ee986
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-10-18 11:42:23 +0200

    Dodano copyright do nowych plików

--- src/games/stendhal/client/actions/RunicAltarAction.java
+++ src/games/stendhal/client/actions/RunicAltarAction.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                 (C) Copyright 2019-2024 - PolanieOnLine                 *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.client.actions;
 
 import games.stendhal.client.gui.j2DClient;
