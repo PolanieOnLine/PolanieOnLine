49715ec41e62a2f67d0fb8bd81def2902cc62ed1
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-12-21 00:16:42 +0100

    Poprawiono dialogi w zadaniu na pierścień rycerza

--- src/games/stendhal/server/maps/quests/PierscienRycerza.java
+++ src/games/stendhal/server/maps/quests/PierscienRycerza.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2010-2021 - Stendhal                    *
+ *                   (C) Copyright 2010-2024 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -54,23 +54,23 @@ public class PierscienRycerza extends AbstractQuest {
 			new ChatAction() {
 				@Override
 				public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-					if (player.isBadBoy()){ 
-						raiser.say("Z twej ręki zginął rycerz! Nie masz tu czego szukać, pozbądź się piętna czaszki. A teraz precz mi z oczu!");
+					if (player.isBadBoy()) { 
+						raiser.say("Twe czyny ciążą na twej duszy jak chmury burzowe nad równiną. Dopóki nie zmażesz piętna, nie znajdziesz u mnie łaski.");
 					} else {
-							if (player.getLevel() >= 250) {
-								if (!player.hasQuest(QUEST_SLOT) || "rejected".equals(player.getQuest(QUEST_SLOT))) {
-									raiser.say("Czyżbyś chciał zdobyć pierścień rycerza? Jesteś zainteresowany?");
-								} else if (player.isQuestCompleted(QUEST_SLOT)) {
-									raiser.say("Jeżeli przyszedłeś po następny pierścień to zapomnij. A teraz nie przeszkadzaj mi w trenowaniu latania.");
-									raiser.setCurrentState(ConversationStates.ATTENDING);
-								}
-							} else  {
-								npc.say("Twój stan społeczny jest zbyt niski aby podjąć te zadanie. Wróć gdy zdobędziesz 250 lvl.");
+						if (player.getLevel() >= 250) {
+							if (!player.hasQuest(QUEST_SLOT) || "rejected".equals(player.getQuest(QUEST_SLOT))) {
+								raiser.say("Czy godny żeś noszenia pierścienia rycerza? Twoja decyzja będzie zapisana w gwiazdach.");
+							} else if (player.isQuestCompleted(QUEST_SLOT)) {
+								raiser.say("Zaszczyt rycerza już posiadasz. Skup się teraz na utrwaleniu swej chwały.");
 								raiser.setCurrentState(ConversationStates.ATTENDING);
 							}
+						} else {
+							npc.say("Twoja siła i mądrość są jeszcze jak młode drzewo na wietrze. Wróć, gdy osiągniesz poziom 250.");
+							raiser.setCurrentState(ConversationStates.ATTENDING);
+						}
 					}
 				}
-			}); 
+			});
 
 		npc.add(ConversationStates.QUEST_OFFERED,
 			ConversationPhrases.YES_MESSAGES, null,
@@ -78,7 +78,7 @@ public class PierscienRycerza extends AbstractQuest {
 			new ChatAction() {
 				@Override
 				public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-					raiser.say("Ale wpierw sprawdzę czy masz wszystkie zadania zrobione nim dostaniesz #listę rzeczy, krórych potrzebuję.");
+					raiser.say("Twoja odwaga zostanie wystawiona na próbę, a duch twój – na szalę. Przygotuj się dobrze. Mogę przygotować dla Ciebie #listę przedmiotów.");
 					player.addKarma(10);
 				}
 			});
@@ -86,7 +86,7 @@ public class PierscienRycerza extends AbstractQuest {
 		npc.add(ConversationStates.QUEST_OFFERED,
 			ConversationPhrases.NO_MESSAGES, null,
 			ConversationStates.IDLE,
-			"Nie to nie.",
+			"Rozumiem. Każdy czas ma swoje przeznaczenie. Wróć, gdy poczujesz gotowość.",
 			new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0));
 	}
 
@@ -96,75 +96,79 @@ public class PierscienRycerza extends AbstractQuest {
 			new AndCondition(new QuestCompletedCondition(MITHRILSHIELD_QUEST_SLOT),
 					 new QuestNotStartedCondition(QUEST_SLOT)),
 			ConversationStates.ATTENDING,
-			"Witaj przyjacielu. Czyż byś chciał dostać #zadanie na pierścień rycerza?",
+			"Witaj, wędrowcze. Pragniesz dowieść swej wartości, zdobywając pierścień rycerza?",
 			null);
 
 		npc.add(ConversationStates.ATTENDING,
-			Arrays.asList("wyposażenie", "wyposazenie","listę"), 
+			Arrays.asList("wyposażenie", "wyposazenie", "listę"), 
 			new AndCondition(
 					 new QuestNotStartedCondition(QUEST_SLOT),
 					 new QuestNotCompletedCondition(MITHRILSHIELD_QUEST_SLOT)),
 			ConversationStates.ATTENDING,
-			"Najpierw zrób zadanie na tarczę z mithrilu, wtedy porozmawiamy o pierścieniu.",
+			"Najpierw wykaż się, wykonując zadanie na tarczę z mithrilu. Potem porozmawiamy o pierścieniu.",
 			null);
 	}
 
 	private void requestItem() {
 		npc.add(ConversationStates.ATTENDING,
-				Arrays.asList("wyposażenie", "wyposazenie","listę"),
-				new AndCondition(
-						new QuestNotStartedCondition(QUEST_SLOT),
-						new QuestCompletedCondition(MITHRILSHIELD_QUEST_SLOT)),
-					ConversationStates.ATTENDING, "Pierścień zdobędziesz przynosząc mi potrzebne #przedmioty.",
-					new SetQuestAction(QUEST_SLOT, "przedmioty"));
+			Arrays.asList("wyposażenie", "wyposazenie", "listę"),
+			new AndCondition(
+					new QuestNotStartedCondition(QUEST_SLOT),
+					new QuestCompletedCondition(MITHRILSHIELD_QUEST_SLOT)),
+			ConversationStates.ATTENDING, "Pierścień zdobyć możesz, jeśli przyniesiesz mi #przedmioty godne rycerza. Oto ich lista:"
+				+ "\n#'40 sztabek żelaza'"
+				+ "\n#'40 sztabek złota'"
+				+ "\n#'30 bryłek mithrilu'"
+				+ "\n#'1 pierścień mieszczanina'"
+				+ "\n#'1 tarcza z czaszką'"
+				+ "\n#'1 spodnie elfickie'\n. Zbierz je wszystkie i wróć, bym mógł cię uhonorować. Gdy będziesz czuć potrzebę przypomnieć listę to wróć i powiedz #przypomnij.",
+			new SetQuestAction(QUEST_SLOT, "przedmioty"));
 
 		final List<ChatAction> reward = new LinkedList<ChatAction>();
-		reward.add(new DropItemAction("sztabka żelaza",40));
-		reward.add(new DropItemAction("sztabka złota",40));
-		reward.add(new DropItemAction("bryłka mithrilu",30));
-		reward.add(new DropItemAction("pierścień mieszczanina",1));
-		reward.add(new DropItemAction("tarcza z czaszką",1));
-		reward.add(new DropItemAction("spodnie elfickie",1));
+		reward.add(new DropItemAction("sztabka żelaza", 40));
+		reward.add(new DropItemAction("sztabka złota", 40));
+		reward.add(new DropItemAction("bryłka mithrilu", 30));
+		reward.add(new DropItemAction("pierścień mieszczanina", 1));
+		reward.add(new DropItemAction("tarcza z czaszką", 1));
+		reward.add(new DropItemAction("spodnie elfickie", 1));
 		reward.add(new EquipItemAction("pierścień rycerza", 1, true));
 		reward.add(new IncreaseXPAction(100000));
 		reward.add(new SetQuestAction(QUEST_SLOT, "done"));
 
-		npc.add(ConversationStates.ATTENDING, Arrays.asList("wyposażenie", "wyposazenie","przedmioty"),
+		npc.add(ConversationStates.ATTENDING, Arrays.asList("wyposażenie", "wyposazenie", "przedmioty"),
 			new AndCondition(new QuestInStateCondition(QUEST_SLOT,"przedmioty"),
-			new PlayerHasItemWithHimCondition("sztabka żelaza",40),
-			new PlayerHasItemWithHimCondition("sztabka złota",40),
-			new PlayerHasItemWithHimCondition("bryłka mithrilu",30),
-			new PlayerHasItemWithHimCondition("pierścień mieszczanina",1),
-			new PlayerHasItemWithHimCondition("tarcza z czaszką",1),
-			new PlayerHasItemWithHimCondition("spodnie elfickie",1)),
-			ConversationStates.ATTENDING, "Widzę, że masz wszystko o co cię prosiłem. A oto twój pierścień rycerza.",
-		new MultipleActions(reward));
+					new PlayerHasItemWithHimCondition("sztabka żelaza", 40),
+					new PlayerHasItemWithHimCondition("sztabka złota", 40),
+					new PlayerHasItemWithHimCondition("bryłka mithrilu", 30),
+					new PlayerHasItemWithHimCondition("pierścień mieszczanina", 1),
+					new PlayerHasItemWithHimCondition("tarcza z czaszką", 1),
+					new PlayerHasItemWithHimCondition("spodnie elfickie", 1)),
+			ConversationStates.ATTENDING, "Widzę, że zebrałeś wszystko, co potrzebne. Oto twój pierścień rycerza. Noś go z dumą, a duchy przodków będą cię strzec.",
+			new MultipleActions(reward));
 
 		npc.add(ConversationStates.ATTENDING, Arrays.asList("wyposażenie", "wyposazenie", "przedmioty", "przypomnij"),
-				new AndCondition(new QuestInStateCondition(QUEST_SLOT,"przedmioty"),
-								 new NotCondition(
-								 new AndCondition(new PlayerHasItemWithHimCondition("sztabka żelaza",40),
-												  new PlayerHasItemWithHimCondition("sztabka złota",40),
-												  new PlayerHasItemWithHimCondition("bryłka mithrilu",30),
-												  new PlayerHasItemWithHimCondition("pierścień mieszczanina",1),
-												  new PlayerHasItemWithHimCondition("tarcza z czaszką",1),
-												  new PlayerHasItemWithHimCondition("spodnie elfickie",1)))),
-				ConversationStates.ATTENDING, "Przynieś mi:\n"
-									+"#'40 żelazo'\n"
-									+"#'40 sztabka złota'\n"
-									+"#'30 bryłka mithrilu'\n"
-									+"#'1 pierścień mieszczanina'\n"
-									+"#'1 tarcza z czaszką'\n"
-									+"#'1 spodnie elfickie'\n"
-									+" Proszę przynieś mi to wszystko naraz."
-									+" Słowo klucz to #'/przedmioty/'.  Dziękuję!", null);
+			new AndCondition(new QuestInStateCondition(QUEST_SLOT,"przedmioty"),
+				new NotCondition(
+				new AndCondition(new PlayerHasItemWithHimCondition("sztabka żelaza", 40),
+						new PlayerHasItemWithHimCondition("sztabka złota", 40),
+						new PlayerHasItemWithHimCondition("bryłka mithrilu", 30),
+						new PlayerHasItemWithHimCondition("pierścień mieszczanina", 1),
+						new PlayerHasItemWithHimCondition("tarcza z czaszką", 1),
+						new PlayerHasItemWithHimCondition("spodnie elfickie", 1)))),
+			ConversationStates.ATTENDING, "Przynieś mi następujące #przedmioty, które dowiodą twej odwagi i męstwa:"
+				+ "\n#'40 sztabek żelaza'"
+				+ "\n#'40 sztabek złota'"
+				+ "\n#'30 bryłek mithrilu'"
+				+ "\n#'1 pierścień mieszczanina'"
+				+ "\n#'1 tarcza z czaszką'"
+				+ "\n#'1 spodnie elfickie'\n. Zbierz je wszystkie i wróć, bym mógł cię uhonorować.", null);
 	}
 
 	@Override
 	public void addToWorld() {
 		fillQuestInfo(
 				"Pierścień Rycerza",
-				"Uporaj się z wyzwaniami, które postawił przed tobą Edgard.",
+				"Uporaj się z wyzwaniami i udowodnij swą odwagę, aby zdobyć pierścień od Edgarda.",
 				true);
 
 		checkLevelHelm(); 
@@ -174,34 +178,41 @@ public class PierscienRycerza extends AbstractQuest {
 
 	@Override
 	public List<String> getHistory(final Player player) {
-			final List<String> res = new ArrayList<String>();
-			if (!player.hasQuest(QUEST_SLOT)) {
-				return res;
-			}
-			final String questState = player.getQuest(QUEST_SLOT);
-			res.add(player.getGenderVerb("Spotkałem") + " Edgarda na terenie Zakonu.");
-			res.add("Zaproponował mi pierścień rycerza wzamian za parę rzeczy.");
-			if ("rejected".equals(questState)) {
-				res.add("Nie potrzebny jest mi pierścień rycerza.");
-				return res;
-			} 
-			if ("start".equals(questState)) {
-				return res;
-			} 
-			res.add("Edgard poprosił, abym " + player.getGenderVerb("dostarczył") + " potrzebne przedmioty. Jeżeli nie będę " + player.getGenderVerb("pamiętał") + " co mam przynieść mam powiedzieć przypomnij.");
-			if ("przedmioty".equals(questState)) {
-				return res;
-			} 
-			res.add("Edgard jest zadowolony z mojej postawy. W zamian " + player.getGenderVerb("dostałem") + " pierścień rycerza.");
-			if (isCompleted(player)) {
-				return res;
-			} 
+		final List<String> res = new ArrayList<String>();
+		if (!player.hasQuest(QUEST_SLOT)) {
+			return res;
+		}
+		final String questState = player.getQuest(QUEST_SLOT);
+		res.add(player.getGenderVerb("Spotkałem") + " Edgarda na terenie Zakonu.");
+		res.add("Zaproponował mi pierścień rycerza w zamian za dostarczenie określonych przedmiotów.");
 
-			// if things have gone wrong and the quest state didn't match any of the above, debug a bit:
-			final List<String> debug = new ArrayList<String>();
-			debug.add("Stan zadania to: " + questState);
-			logger.error("Historia nie pasuje do stanu poszukiwania " + questState);
-			return debug;
+		if ("rejected".equals(questState)) {
+			res.add("Odrzuciłem ofertę Edgarda i uznałem, że pierścień rycerza nie jest mi potrzebny.");
+			return res;
+		}
+
+		if ("start".equals(questState)) {
+			return res;
+		}
+
+		res.add("Edgard poprosił, abym " + player.getGenderVerb("dostarczył") + " potrzebne przedmioty. Jeśli zapomnę, co mam przynieść, mogę powiedzieć 'przypomnij'.");
+		
+		if ("przedmioty".equals(questState)) {
+			return res;
+		}
+
+		res.add("Ukończyłem zadanie Edgarda. W nagrodę " + player.getGenderVerb("otrzymałem") + " pierścień rycerza, symbol mojej odwagi i determinacji.");
+		
+		if (isCompleted(player)) {
+			res.add("Zadanie zakończone. Mogę teraz nosić pierścień rycerza z dumą.");
+			return res;
+		}
+
+		// if things have gone wrong and the quest state didn't match any of the above, debug a bit:
+		final List<String> debug = new ArrayList<String>();
+		debug.add("Stan zadania to: " + questState);
+		logger.error("Historia nie pasuje do stanu poszukiwania " + questState);
+		return debug;
 	}
 
 	@Override
