7af44d33a4f4f7f3866a60e2fc04ff6382836ff3
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2025-10-11 15:15:55 +0200

    Move reserve set to detachable window

---
+++ src/games/stendhal/client/gui/ReserveSetWindow.java
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.gui;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.JComponent;
+
+class ReserveSetWindow extends InternalManagedWindow {
+	/**
+	 * serial version uid.
+	 */
+	private static final long serialVersionUID = -6792704385484299338L;
+
+	private final Character owner;
+	private boolean added;
+
+	ReserveSetWindow(Character owner, JComponent content) {
+		super("reserve_set", "Schowek");
+		this.owner = owner;
+		setContent(content);
+		setCloseable(true);
+		setVisible(false);
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent event) {
+				owner.onReserveWindowVisibilityChange(true);
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent event) {
+				owner.onReserveWindowVisibilityChange(false);
+			}
+		});
+	}
+
+	void attach() {
+		if (added) {
+			return;
+		}
+		added = true;
+		j2DClient.get().addWindow(this);
+	}
+
+	void showBeside(Component anchor) {
+		if (!added) {
+			return;
+		}
+		setVisible(true);
+		setMinimized(false);
+		Point location = anchor.getLocation();
+		location.translate(anchor.getWidth() + 4, 0);
+		moveTo(location.x, location.y);
+		raise();
+	}
+}
