83aaa9eb94c207c6198d11da22b958f8bf115fd6
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2025-10-11 08:00:05 +0200

    Reindent set change implementation with tabs

--- src/games/stendhal/server/actions/CommandCenter.java
+++ src/games/stendhal/server/actions/CommandCenter.java
@@ -32,6 +32,7 @@ import games.stendhal.server.actions.equip.DisplaceAction;
 import games.stendhal.server.actions.equip.DropAction;
 import games.stendhal.server.actions.equip.EquipAction;
 import games.stendhal.server.actions.equip.ReorderAction;
+import games.stendhal.server.actions.equip.SetChangeAction;
 import games.stendhal.server.actions.move.AutoWalkAction;
 import games.stendhal.server.actions.move.ConditionalStopAction;
 import games.stendhal.server.actions.move.FaceAction;
@@ -106,6 +107,7 @@ public class CommandCenter {
 		DisplaceAction.register();
 		DropAction.register();
 		EquipAction.register();
+		SetChangeAction.register();
 		FaceAction.register();
 		ForsakeAction.register();
 		GroupManagementAction.register();
