ba7564ca5cc5225182a233944b8893227214442b
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2025-10-14 21:59:27 +0200

    Track glyph bonuses per item when equipping

--- src/games/stendhal/server/entity/item/Glyph.java
+++ src/games/stendhal/server/entity/item/Glyph.java
@@ -7,6 +7,12 @@ import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPObject;
 
 public class Glyph extends Item {
+	private static final String ATTRIBUTE_ATTACK = "skill_atk";
+	private static final String ATTRIBUTE_ATTACK_ALIAS = "atk";
+	private static final String ATTRIBUTE_HEALTH = "health";
+	private static final String ATTRIBUTE_APPLIED_ATTACK = "applied_glyph_attack_bonus";
+	private static final String ATTRIBUTE_APPLIED_HEALTH = "applied_glyph_health_bonus";
+
 	public Glyph(final String name, final String clazz, final String subclass,
 			final Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
@@ -16,20 +22,12 @@ public class Glyph extends Item {
 		super(glyph);
 	}
 
-	private int getAttackBonus() {
-		return has("skill_atk") ? getInt("skill_atk") : 0;
-	}
-
-	private int getHealthBonus() {
-		return has("health") ? getInt("health") : 0;
-	}
-
 	@Override
 	public boolean onEquipped(final RPEntity entity, final String slot) {
 		if (entity instanceof Player) {
 			Player player = (Player) entity;
-			player.setAtk(player.getAtk() + getAttackBonus());
-			player.setBaseHP(player.getBaseHP() + getHealthBonus());
+			applyAttackBonus(player);
+			applyHealthBonus(player);
 		}
 
 		return super.onEquipped(entity, slot);
@@ -46,19 +44,87 @@ public class Glyph extends Item {
 		return super.onUnequipped();
 	}
 
-	private void reduceHP(Player player) {
-		int healthBonus = getHealthBonus();
-		if (healthBonus == 0) {
-			return;
+	private int getAttackBonus() {
+		if (has(ATTRIBUTE_ATTACK)) {
+			return getInt(ATTRIBUTE_ATTACK);
 		}
-		int reduceHP = Math.max(player.getBaseHP() - healthBonus, 0);
-		if (player.getHP() == player.getBaseHP() || player.getHP() > reduceHP) {
-			player.setHP(reduceHP);
+		if (has(ATTRIBUTE_ATTACK_ALIAS)) {
+			return getInt(ATTRIBUTE_ATTACK_ALIAS);
 		}
-		player.setBaseHP(reduceHP);
+		return 0;
 	}
 
-	private void reduceAtk(Player player) {
-		player.setAtk(player.getAtk() - getAttackBonus());
+	private int getHealthBonus() {
+		return has(ATTRIBUTE_HEALTH) ? getInt(ATTRIBUTE_HEALTH) : 0;
+	}
+
+	private void applyAttackBonus(final Player player) {
+		int attackBonus = getAttackBonus();
+		if (attackBonus == 0) {
+			forgetAppliedAttackBonus();
+			return;
+		}
+		player.setAtk(player.getAtk() + attackBonus);
+		rememberAppliedAttackBonus(attackBonus);
+	}
+
+	private void applyHealthBonus(final Player player) {
+		int healthBonus = getHealthBonus();
+		if (healthBonus == 0) {
+			forgetAppliedHealthBonus();
+			return;
+		}
+		player.setBaseHP(player.getBaseHP() + healthBonus);
+		rememberAppliedHealthBonus(healthBonus);
+	}
+
+	private void reduceAtk(final Player player) {
+		int appliedAttackBonus = getAppliedAttackBonus();
+		if (appliedAttackBonus == 0) {
+			return;
+		}
+		player.setAtk(player.getAtk() - appliedAttackBonus);
+		forgetAppliedAttackBonus();
+	}
+
+	private void reduceHP(final Player player) {
+		int appliedHealthBonus = getAppliedHealthBonus();
+		if (appliedHealthBonus == 0) {
+			return;
+		}
+		int newBaseHP = Math.max(player.getBaseHP() - appliedHealthBonus, 0);
+		player.setBaseHP(newBaseHP);
+		if (player.getHP() > newBaseHP) {
+			player.setHP(newBaseHP);
+		}
+		forgetAppliedHealthBonus();
+	}
+
+	private void rememberAppliedAttackBonus(final int bonus) {
+		put(ATTRIBUTE_APPLIED_ATTACK, bonus);
+	}
+
+	private void rememberAppliedHealthBonus(final int bonus) {
+		put(ATTRIBUTE_APPLIED_HEALTH, bonus);
+	}
+
+	private void forgetAppliedAttackBonus() {
+		if (has(ATTRIBUTE_APPLIED_ATTACK)) {
+			remove(ATTRIBUTE_APPLIED_ATTACK);
+		}
+	}
+
+	private void forgetAppliedHealthBonus() {
+		if (has(ATTRIBUTE_APPLIED_HEALTH)) {
+			remove(ATTRIBUTE_APPLIED_HEALTH);
+		}
+	}
+
+	private int getAppliedAttackBonus() {
+		return has(ATTRIBUTE_APPLIED_ATTACK) ? getInt(ATTRIBUTE_APPLIED_ATTACK) : 0;
+	}
+
+	private int getAppliedHealthBonus() {
+		return has(ATTRIBUTE_APPLIED_HEALTH) ? getInt(ATTRIBUTE_APPLIED_HEALTH) : 0;
 	}
 }
