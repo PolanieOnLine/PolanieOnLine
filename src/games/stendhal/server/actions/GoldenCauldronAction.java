bfa96938599b8dc265c2069e57e2f79e0b4f5f06
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2025-10-17 14:54:45 +0200

    Rebuild golden cauldron interactions

--- src/games/stendhal/server/actions/GoldenCauldronAction.java
+++ src/games/stendhal/server/actions/GoldenCauldronAction.java
@@ -1,14 +1,14 @@
 package games.stendhal.server.actions;
 
-import static games.stendhal.common.constants.Actions.TARGET;
-import static games.stendhal.common.constants.Actions.TARGET_PATH;
-
 import games.stendhal.server.entity.Entity;
-import games.stendhal.server.entity.mapstuff.useable.GoldenCauldronEntity;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.mapstuff.useable.GoldenCauldronEntity;
 import games.stendhal.server.util.EntityHelper;
 import marauroa.common.game.RPAction;
 
+/**
+ * Handles client actions directed at Draconia's golden cauldron.
+ */
 public class GoldenCauldronAction implements ActionListener {
 	private static final String TYPE = "golden_cauldron";
 
@@ -18,40 +18,27 @@ public class GoldenCauldronAction implements ActionListener {
 
 	@Override
 	public void onAction(final Player player, final RPAction action) {
-		final String actionZone = action.get("zone");
-		if (actionZone != null && !actionZone.equals(player.getZone().getName())) {
-			return;
-		}
-
-		final String command = action.get("action");
-		if (command == null) {
-			return;
-		}
-
 		final GoldenCauldronEntity cauldron = resolveTarget(player, action);
 		if (cauldron == null) {
 			return;
 		}
 
-		switch (command) {
-		case "mix":
-			cauldron.mix(player);
-			break;
-		case "close":
+		final String command = action.get("action");
+		if ("open".equals(command)) {
+			cauldron.onUsed(player);
+		} else if ("close".equals(command)) {
 			cauldron.close(player);
-			break;
-		default:
-			break;
+		} else if ("mix".equals(command)) {
+			cauldron.mix(player);
 		}
 	}
 
 	private GoldenCauldronEntity resolveTarget(final Player player, final RPAction action) {
-		Entity entity = null;
-		if (action.has(TARGET_PATH)) {
-			entity = EntityHelper.getEntityFromPath(player, action.getList(TARGET_PATH));
-		} else if (action.has(TARGET)) {
-			entity = EntityHelper.entityFromTargetName(action.get(TARGET), player);
+		if (!action.has(games.stendhal.common.constants.Actions.TARGET_PATH)) {
+			return null;
 		}
+		final Entity entity = EntityHelper.getEntityFromPath(player,
+				action.getList(games.stendhal.common.constants.Actions.TARGET_PATH));
 		if (entity instanceof GoldenCauldronEntity) {
 			return (GoldenCauldronEntity) entity;
 		}
