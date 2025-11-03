84dc71bb2bfeb441c38ecbdefa427cc885abe40d
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2025-11-03 18:59:49 +0100

    Add toggleable buddy search filter

--- src/games/stendhal/client/gui/buddies/BuddyPanelController.java
+++ src/games/stendhal/client/gui/buddies/BuddyPanelController.java
@@ -12,7 +12,13 @@
  ***************************************************************************/
 package games.stendhal.client.gui.buddies;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Insets;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.Map;
@@ -20,12 +26,17 @@ import java.util.Map.Entry;
 
 import javax.swing.JComponent;
 import javax.swing.JList;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+import javax.swing.Icon;
+
+import java.awt.event.ActionEvent;
 
 import games.stendhal.client.gui.layout.SBoxLayout;
 import games.stendhal.client.gui.layout.SLayout;
@@ -39,6 +50,37 @@ public final class BuddyPanelController implements PropertyChangeListener {
 	 * will need the panel anyway, so it's OK to instantiate it right away.
 	 */
 	private static final BuddyPanelController instance = new BuddyPanelController();
+
+	private static final Icon SEARCH_ICON = new Icon() {
+		@Override
+		public void paintIcon(Component c, Graphics g, int x, int y) {
+			Graphics2D graphics = (Graphics2D) g.create();
+			try {
+				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				graphics.setColor(new Color(70, 70, 70));
+				graphics.setStroke(new BasicStroke(1.6f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+				int diameter = 8;
+				int ringX = x + 2;
+				int ringY = y + 2;
+				graphics.drawOval(ringX, ringY, diameter, diameter);
+				int handleStartX = ringX + diameter - 1;
+				int handleStartY = ringY + diameter - 1;
+				graphics.drawLine(handleStartX, handleStartY, x + getIconWidth() - 2, y + getIconHeight() - 2);
+			} finally {
+				graphics.dispose();
+			}
+		}
+
+		@Override
+		public int getIconWidth() {
+			return 14;
+		}
+
+		@Override
+		public int getIconHeight() {
+			return 14;
+		}
+	};
 
 	private final JComponent buddyPanel;
 	private final BuddyListModel model;
@@ -54,10 +96,15 @@ public final class BuddyPanelController implements PropertyChangeListener {
 		model = new BuddyListModel();
 
 		JComponent filterRow = SBoxLayout.createContainer(SBoxLayout.HORIZONTAL, SBoxLayout.COMMON_PADDING);
-		JLabel filterLabel = new JLabel("Szukaj znajomych:");
-		filterRow.add(filterLabel);
+		final JToggleButton filterToggle = new JToggleButton(SEARCH_ICON);
+		filterToggle.setMargin(new Insets(2, 4, 2, 4));
+		filterToggle.setText(null);
+		filterToggle.setFocusable(false);
+		filterToggle.setToolTipText("Wyszukaj znajomego");
+		filterRow.add(filterToggle);
 		final JTextField filterField = new JTextField();
 		filterField.setColumns(12);
+		filterField.setVisible(false);
 		filterField.setToolTipText("Filtruj listę znajomych według nazwy");
 		filterField.getDocument().addDocumentListener(new DocumentListener() {
 			@Override
@@ -73,6 +120,45 @@ public final class BuddyPanelController implements PropertyChangeListener {
 			@Override
 			public void changedUpdate(DocumentEvent e) {
 				model.setFilter(filterField.getText());
+			}
+		});
+		final Runnable hideFilter = new Runnable() {
+			@Override
+			public void run() {
+				if (!filterField.getText().isEmpty()) {
+					filterField.setText("");
+				}
+				filterField.setVisible(false);
+				filterRow.revalidate();
+				filterRow.repaint();
+			}
+		};
+		final Runnable showFilter = new Runnable() {
+			@Override
+			public void run() {
+				filterField.setVisible(true);
+				filterRow.revalidate();
+				filterRow.repaint();
+				filterField.requestFocusInWindow();
+			}
+		};
+		filterField.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke("ESCAPE"), "buddyFilterHide");
+		filterField.getActionMap().put("buddyFilterHide", new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (filterToggle.isSelected()) {
+					filterToggle.setSelected(false);
+					hideFilter.run();
+				}
+			}
+		});
+		filterToggle.addActionListener(e -> {
+			if (filterToggle.isSelected()) {
+				showFilter.run();
+			} else {
+				hideFilter.run();
 			}
 		});
 		filterRow.add(filterField, SLayout.EXPAND_X);
