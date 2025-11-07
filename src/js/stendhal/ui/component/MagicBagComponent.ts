1e7ca6cc9fc46751ea15a5c45ce6f9e7b60c8160
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2025-11-07 03:36:25 +0100

    Naprawiono w webkliencie magiczną torbę

--- src/js/stendhal/ui/component/MagicBagComponent.ts
+++ src/js/stendhal/ui/component/MagicBagComponent.ts
@@ -4,8 +4,8 @@ declare var marauroa: any;
 
 
 export class MagicBagComponent extends ItemInventoryComponent {
-
-	private slotSize = "6 1";
+	private readonly defaultSlotSize = "6 1";
+	private slotSize = this.defaultSlotSize;
 
 	constructor(object: any, slot: string, sizeX: number, sizeY: number, quickPickup: boolean, defaultImage?: string) {
 		super(object, slot, sizeX, sizeY, quickPickup, defaultImage);
@@ -20,18 +20,20 @@ export class MagicBagComponent extends ItemInventoryComponent {
 			return;
 		}
 
-		let size = features["magicbag"];
-
-		if (size) {
+		const sizeFeature = features["magicbag"];
+		if (sizeFeature !== undefined && sizeFeature !== null) {
 			if (!this.isVisible()) {
 				this.setVisible(true);
 			}
-			if (this.slotSize != size) {
-				this.slotSize = size;
-				let sizeArray = size.split(" ");
-				let sizeX = parseInt(sizeArray[0], 10);
-				let sizeY = parseInt(sizeArray[1], 10);
-				this.resize(sizeX, sizeY);
+			const normalizedSize = sizeFeature === "" ? this.defaultSlotSize : String(sizeFeature).trim();
+			if (this.slotSize != normalizedSize) {
+				this.slotSize = normalizedSize;
+				const sizeArray = normalizedSize.split(/\s+/);
+				const sizeX = parseInt(sizeArray[0], 10);
+				const sizeY = parseInt(sizeArray[1], 10);
+				if (!Number.isNaN(sizeX) && !Number.isNaN(sizeY)) {
+					this.resize(sizeX, sizeY);
+				}
 			}
 		} else if (this.isVisible()) {
 			this.setVisible(false);
@@ -39,7 +41,6 @@ export class MagicBagComponent extends ItemInventoryComponent {
 
 		super.update();
 	}
-
 
 	private resize(sizeX: number, sizeY: number) {
 		super.setSize(sizeX, sizeY);
