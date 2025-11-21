28f348e0a6b975b15ddb916bd8e8c37c7a4937e7
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2025-11-21 20:51:07 +0100

    Naprawiono naliczanie bonusów z glifów

--- src/games/stendhal/server/entity/item/Glyph.java
+++ src/games/stendhal/server/entity/item/Glyph.java
@@ -7,8 +7,7 @@ import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPObject;
 
 public class Glyph extends Item {
-	public Glyph(final String name, final String clazz, final String subclass,
-			final Map<String, String> attributes) {
+	public Glyph(final String name, final String clazz, final String subclass, final Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 	}
 
@@ -16,28 +15,19 @@ public class Glyph extends Item {
 		super(glyph);
 	}
 
-	private int getAttackBonus(final Player player) {
-		int atk = 0;
-		for (final Item equip: player.getAllEquippedGlyphs()) {
-			atk += equip.has("skill_atk") ? equip.getInt("skill_atk") : 0;
-		}
-		return atk;
+	private int getAttackBonus(final Item glyph) {
+		return glyph.has("skill_atk") ? glyph.getInt("skill_atk") : 0;
 	}
 
-	private int getHealthBonus(final Player player) {
-		int hp = 0;
-		for (final Item equip: player.getAllEquippedGlyphs()) {
-			hp += equip.has("health") ? equip.getInt("health") : 0;
-		}
-		return hp;
+	private int getHealthBonus(final Item glyph) {
+		return glyph.has("health") ? glyph.getInt("health") : 0;
 	}
 
 	@Override
 	public boolean onEquipped(final RPEntity entity, final String slot) {
 		if (entity instanceof Player) {
-			Player player = (Player) entity;
-			entity.setAtk(entity.getAtk() + getAttackBonus(player));
-			entity.setBaseHP(entity.getBaseHP() + getHealthBonus(player));
+			entity.setAtk(entity.getAtk() + getAttackBonus(this));
+			entity.setBaseHP(entity.getBaseHP() + getHealthBonus(this));
 		}
 
 		return super.onEquipped(entity, slot);
@@ -55,14 +45,14 @@ public class Glyph extends Item {
 	}
 
 	private void reduceHP(Player player) {
-		int reduceHP = player.getBaseHP() - getHealthBonus(player);
+		int reduceHP = player.getBaseHP() - getHealthBonus(this);
 		if (player.getHP() == player.getBaseHP() || player.getHP() > reduceHP) {
 			player.setHP(reduceHP);
 		}
 		player.setBaseHP(reduceHP);
 	}
-	
+
 	private void reduceAtk(Player player) {
-		player.setAtk(player.getAtk() - getAttackBonus(player));
+		player.setAtk(player.getAtk() - getAttackBonus(this));
 	}
 }
