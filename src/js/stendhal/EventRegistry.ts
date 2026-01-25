1b44e99b13aa83079391eabfecbc96faa245acb5
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2026-01-25 19:59:42 +0100

    Add global fog visual effects to clients

---
+++ src/js/stendhal/EventRegistry.ts
@@ -0,0 +1,193 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2024 - Stendhal                    *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+declare var marauroa: any;
+declare var stendhal: any;
+
+import { RPEntity } from "./entity/RPEntity";
+import { RPObject } from "./entity/RPObject";
+
+import { BestiaryEvent } from "./event/BestiaryEvent";
+import { ChatOptionsEvent } from "./event/ChatOptionsEvent";
+import { ExamineEvent } from "./event/ExamineEvent";
+import { GlobalVisualEffectEvent } from "./event/GlobalVisualEffectEvent";
+import { GroupChangeEvent } from "./event/GroupChangeEvent";
+import { GroupInviteEvent } from "./event/GroupInviteEvent";
+import { PlayerLoggedOnEvent } from "./event/PlayerLoggedOnEvent";
+import { PlayerLoggedOutEvent } from "./event/PlayerLoggedOutEvent";
+import { ProgressStatusEvent } from "./event/ProgressStatusEvent";
+import { RPEvent } from "./event/RPEvent";
+import { ShowItemListEvent } from "./event/ShowItemListEvent";
+import { ShowOutfitListEvent } from "./event/ShowOutfitListEvent";
+import { SoundEvent } from "./event/SoundEvent";
+import { TradeEvent } from "./event/TradeEvent";
+import { ViewChangeEvent } from "./event/ViewChangeEvent";
+
+import { Chat } from "./util/Chat";
+
+
+export class EventRegistry {
+
+	private initialized = false;
+
+	/** Singleton instance. */
+	private static instance: EventRegistry;
+
+
+	/**
+	 * Retrieves singleton instance.
+	 */
+	static get(): EventRegistry {
+		if (!EventRegistry.instance) {
+			EventRegistry.instance = new EventRegistry();
+		}
+		return EventRegistry.instance;
+	}
+
+	/**
+	 * Hidden singleton constructor.
+	 */
+	private constructor() {
+		// do nothing
+	}
+
+	/**
+	 * Registers standard events.
+	 */
+	init() {
+		if (this.initialized) {
+			console.warn("tried to re-initialize EventRegistry");
+			return;
+		}
+		this.initialized = true;
+
+		this.register("chat_options", new ChatOptionsEvent());
+		this.register("examine", new ExamineEvent());
+		this.register("group_change_event", new GroupChangeEvent());
+		this.register("group_invite_event", new GroupInviteEvent());
+		this.register("progress_status_event", new ProgressStatusEvent());
+		this.register("trade_state_change_event", new TradeEvent());
+
+		this.register("attack", {
+			execute: function(entity: RPEntity) {
+				var target = entity.getAttackTarget();
+				if (!target) {
+					return;
+				}
+				if (this.hasOwnProperty("hit")) {
+					var damage = parseInt(this["damage"], 10);
+					if (damage !== 0) {
+						target.onDamaged(entity, damage);
+					} else {
+						target.onBlocked(entity);
+					}
+				} else {
+					target.onMissed(entity);
+				}
+				entity.onAttackPerformed(parseInt(this["type"], 10), this.hasOwnProperty("ranged"), this["weapon"]);
+			}
+		}); // attack
+
+		this.register("bestiary", new BestiaryEvent());
+
+		this.register("global_visual_effect", new GlobalVisualEffectEvent());
+
+		this.register("image_event", {
+			execute: function(rpobject: RPObject) {
+				// TODO: new ImageEffectEvent();
+				console.log("image_event", this, rpobject);
+			}
+		}); // image_event
+
+		this.register("player_logged_on", new PlayerLoggedOnEvent());
+		this.register("player_logged_out", new PlayerLoggedOutEvent());
+
+		this.register("private_text", {
+			soundTextEvents: {
+				"privmsg": true,
+				"support": true,
+				"tutorial": true
+			},
+
+			execute: function(rpobject: RPObject) {
+				const ttype = this["texttype"].toLowerCase();
+				const msg = this["text"].replace(/\\r\\n/g, "\n").replace(/\\r/g, "\n");
+
+				let profile;
+				if (this.hasOwnProperty("profile")) {
+					profile = this["profile"];
+				} else if (ttype === "tutorial") {
+					profile = "floattingladynpc";
+				}
+
+				if (ttype === "server" && msg.includes("\n")) {
+					Chat.log(ttype, msg.split("\n"), undefined, profile);
+				} else {
+					// scene settings messages should not disturb playing, just create some atmosphere
+					const headed = ttype !== "scene_setting";
+					Chat.log(ttype, msg, undefined, profile, headed);
+				}
+
+				// play notification sound
+				const notif = stendhal.config.get("chat.private.sound");
+				if (notif && this.soundTextEvents[ttype]) {
+					stendhal.sound.playGlobalizedEffect(notif);
+				}
+			}
+		}); // private_text
+
+		this.register("reached_achievement", {
+			execute: function(rpobject: RPObject) {
+				stendhal.ui.gamewindow.addAchievementNotif(this["category"], this["title"], this["description"]);
+			}
+		}); // reached_achievement
+
+		this.register("show_item_list", new ShowItemListEvent());
+		this.register("show_outfit_list", new ShowOutfitListEvent());
+
+		this.register("sound_event", new SoundEvent());
+
+		this.register("text", {
+			execute: function(entity: RPEntity) {
+				if (this.hasOwnProperty("range")) {
+					entity.say(this["text"], this["range"]);
+				} else {
+					entity.say(this["text"]);
+				}
+			}
+		}); // text
+
+		this.register("transition_graph", {
+			execute: function(rpobject: RPObject) {
+				// TODO: new TransitionGraphEvent();
+			}
+		}); // transition_graph
+
+		this.register("view_change", new ViewChangeEvent());
+	}
+
+	/**
+	 * Registers a new event.
+	 *
+	 * @param _type
+	 *     String identifier.
+	 * @param _event
+	 *     Event to be registered.
+	 */
+	register(_type: string, _event: any) {
+		if (_event instanceof RPEvent) {
+			marauroa.rpeventFactory[_type] = _event;
+		} else {
+			marauroa.rpeventFactory[_type] =
+					marauroa.util.fromProto(marauroa.rpeventFactory["_default"], _event);
+		}
+	}
+}
--- src/js/stendhal/event/GlobalVisualEffectEvent.ts
+++ src/js/stendhal/event/GlobalVisualEffectEvent.ts
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2005-2026 - Stendhal                    *
+ *                   (C) Copyright 2003-2025 - Stendhal                    *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -9,12 +9,21 @@
  *                                                                         *
  ***************************************************************************/
 
-import { RPEvent } from "marauroa"
+import { singletons } from "../SingletonRepo";
+import { RPEvent } from "./RPEvent";
 
+/**
+ * An event that triggers a full-screen visual effect.
+ */
 export class GlobalVisualEffectEvent extends RPEvent {
+	public effect_name!: string;
+	public duration!: number;
+	public strength?: number;
 
-	public execute(_entity: any): void {
-		// not implemented
+	execute(_entity: any): void {
+		const name = this["effect_name"];
+		const duration = Number(this["duration"]);
+		const strength = this.hasOwnProperty("strength") ? Number(this["strength"]) : undefined;
+		singletons.getGlobalVisualEffectRenderer().addEffect(name, duration, strength);
 	}
-
-};
+}
