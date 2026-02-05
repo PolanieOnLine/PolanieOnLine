6ffd8c8d555e41e399dfd2a1ae2974d43ab384cb
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2026-02-05 14:47:52 +0100

    replaced tileset loader "ImagePreloader" as ImageCache using ImageBitmap

--- src/js/stendhal/landscape/IndividualTilesetRenderingStrategy.ts
+++ src/js/stendhal/landscape/IndividualTilesetRenderingStrategy.ts
@@ -12,7 +12,7 @@
 import { stendhal } from "../stendhal";
 
 import { LandscapeRenderingStrategy } from "./LandscapeRenderingStrategy";
-import { ImagePreloader } from "../data/ImagePreloader";
+import { ImageCache } from "../sprite/image/ImageCache";
 import { Chat } from "../util/Chat";
 import { TileMap } from "../data/TileMap";
 import { Canvas, RenderingContext2D } from "util/Types";
@@ -23,6 +23,8 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 
 	private targetTileWidth = 32;
 	private targetTileHeight = 32;
+	private imageCache = new ImageCache();
+	private map!: TileMap;
 	private tileOverlap = 0;
 	private overlapOffset = 0;
 	private effectiveScale = 1;
@@ -34,16 +36,18 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 		}, 1000);
 	}
 
-	public onMapLoaded(_map: TileMap): void {
+	public onMapLoaded(map: TileMap): void {
 		// do nothing
 		console.log("Using IndividualTilesetRenderingStrategy.")
+		this.map = map;
+		this.imageCache.close();
+		this.imageCache = new ImageCache();
+		this.imageCache.load(map.tilesetFilenames);
 	}
 
 	public onTilesetLoaded(): void {
-		new ImagePreloader(stendhal.data.map.tilesetFilenames, function() {
-			let body = document.getElementById("body")!;
-			body.style.cursor = "auto";
-		});
+		let body = document.getElementById("body")!;
+		body.style.cursor = "auto";
 	}
 
 	public render(
@@ -53,8 +57,8 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 		this.targetTileWidth = targetTileWidth;
 		this.targetTileHeight = targetTileHeight;
 
-		for (var drawingLayer = 0; drawingLayer < stendhal.data.map.layers.length; drawingLayer++) {
-			var name = stendhal.data.map.layerNames[drawingLayer];
+		for (var drawingLayer=0; drawingLayer < this.map.layers.length; drawingLayer++) {
+			var name = this.map.layerNames[drawingLayer];
 			if (name !== "protection" && name !== "collision" && name !== "objects"
 				&& name !== "blend_ground" && name !== "blend_roof") {
 				this.paintLayer(canvas, drawingLayer, tileOffsetX, tileOffsetY);
@@ -67,7 +71,7 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 
 	private paintLayer(canvas: Canvas, drawingLayer: number,
 		tileOffsetX: number, tileOffsetY: number) {
-		const layer = stendhal.data.map.layers[drawingLayer];
+		const layer = this.map.layers[drawingLayer];
 		const renderScale = typeof (stendhal.ui?.gamewindow?.getTileScale) === "function"
 			? stendhal.ui.gamewindow.getTileScale()
 			: 1;
@@ -90,26 +94,27 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 		this.tileOverlap = tileOverlap;
 		this.overlapOffset = overlapOffset;
 		this.effectiveScale = clampedScale * pixelRatio;
-		const yMax = Math.min(tileOffsetY + viewportSize.height / this.targetTileHeight + 1, stendhal.data.map.zoneSizeY);
-		const xMax = Math.min(tileOffsetX + viewportSize.width / this.targetTileWidth + 1, stendhal.data.map.zoneSizeX);
+		const yMax = Math.min(tileOffsetY + viewportSize.height / this.targetTileHeight + 1, this.map.zoneSizeY);
+		const xMax = Math.min(tileOffsetX + viewportSize.width / this.targetTileWidth + 1, this.map.zoneSizeX);
 		let ctx = canvas.getContext("2d")! as RenderingContext2D;
 		ctx.imageSmoothingEnabled = false;
 		ctx.imageSmoothingQuality = "low";
 
 		for (let y = tileOffsetY; y < yMax; y++) {
 			for (let x = tileOffsetX; x < xMax; x++) {
-				let gid = layer[y * stendhal.data.map.zoneSizeX + x];
+				let gid = layer[y * this.map.zoneSizeX + x];
 				const flip = gid & 0xE0000000;
 				gid &= 0x1FFFFFFF;
 
 				if (gid > 0) {
-					const tileset = stendhal.data.map.getTilesetForGid(gid);
-					const base = stendhal.data.map.firstgids[tileset];
+					const tileset = this.map.getTilesetForGid(gid);
+					const base = this.map.firstgids[tileset];
 					const idx = gid - base;
 
 					try {
-						if (stendhal.data.map.aImages[tileset].height > 0) {
-							this.drawTile(ctx, stendhal.data.map.aImages[tileset], idx, x, y, flip);
+						let image = this.imageCache.images[this.map.tilesetFilenames[tileset]];
+						if (image) {
+							this.drawTile(ctx, image, idx, x, y, flip);
 						}
 					} catch (e) {
 						console.error(e);
@@ -119,9 +124,9 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 		}
 	}
 
-	private drawTile(ctx: RenderingContext2D, tileset: HTMLImageElement, idx: number, x: number, y: number, flip = 0) {
+	private drawTile(ctx: RenderingContext2D, tileset: ImageBitmap, idx: number, x: number, y: number, flip = 0) {
 		const tilesetWidth = tileset.width;
-		const tilesPerRow = Math.floor(tilesetWidth / stendhal.data.map.tileWidth);
+		const tilesPerRow = Math.floor(tilesetWidth / this.map.tileWidth);
 		const pixelX = this.snapToPixel(x * this.targetTileWidth - this.overlapOffset);
 		const pixelY = this.snapToPixel(y * this.targetTileHeight - this.overlapOffset);
 
@@ -129,9 +134,9 @@ export class IndividualTilesetRenderingStrategy extends LandscapeRenderingStrate
 		const drawTileHeight = this.snapToPixel(this.targetTileHeight + this.tileOverlap);
 		if (flip === 0) {
 			ctx.drawImage(tileset,
-				(idx % tilesPerRow) * stendhal.data.map.tileWidth,
-				Math.floor(idx / tilesPerRow) * stendhal.data.map.tileHeight,
-				stendhal.data.map.tileWidth, stendhal.data.map.tileHeight,
+				(idx % tilesPerRow) * this.map.tileWidth,
+				Math.floor(idx / tilesPerRow) * this.map.tileHeight,
+				this.map.tileWidth, this.map.tileHeight,
 				pixelX, pixelY,
 				drawTileWidth, drawTileHeight);
 		} else {
