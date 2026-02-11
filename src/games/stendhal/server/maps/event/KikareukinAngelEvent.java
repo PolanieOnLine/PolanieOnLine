6cc7f06a5b3fd719ac81b541442f9841161a0143
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2026-02-11 19:03:15 +0100

    Customize Kikareukin event messages

---
+++ src/games/stendhal/server/maps/event/KikareukinAngelEvent.java
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *                    Copyright © 2026 - PolanieOnLine                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.event;
+
+import org.apache.log4j.Logger;
+
+public final class KikareukinAngelEvent extends ConfiguredMapEvent {
+	private static final Logger LOGGER = Logger.getLogger(KikareukinAngelEvent.class);
+
+	public KikareukinAngelEvent() {
+		super(LOGGER, MapEventConfigLoader.load(MapEventConfigLoader.KIKAREUKIN_ANGEL_PREVIEW));
+	}
+
+	@Override
+	protected String getStartAnnouncementMessage() {
+		return "Nad Kikareukin pękły chmury - anielskie zastępy schodzą na wyspy!";
+	}
+
+	@Override
+	protected String getStopAnnouncementMessage() {
+		return "Nad Kikareukin zapadła cisza; chmury i wyspy są na chwilę bezpieczne.";
+	}
+}
