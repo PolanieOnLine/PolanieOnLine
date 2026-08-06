7d00fcc7eafa2a8a4695a133a3ef1ca1d778ea23
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-06 19:04:43 +0200

    test(creature): cover armor fallback and override

---
+++ tests/games/stendhal/server/entity/creature/CreatureArmorTest.java
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2026 - Stendhal                    *
+ ***************************************************************************/
+package games.stendhal.server.entity.creature;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import utilities.RPClass.CreatureTestHelper;
+
+public class CreatureArmorTest {
+	@BeforeClass
+	public static void generateRPClasses() {
+		CreatureTestHelper.generateRPClasses();
+	}
+
+	@Test
+	public void defenseIsDefaultArmorScore() {
+		final Creature creature = new Creature();
+		creature.setDef(55);
+
+		assertEquals(55, creature.getArmor());
+	}
+
+	@Test
+	public void explicitArmorOverridesDefense() {
+		final Creature creature = new Creature();
+		creature.setDef(55);
+		creature.setArmor(12);
+
+		assertEquals(12, creature.getArmor());
+	}
+
+	@Test
+	public void negativeArmorOverrideIsClamped() {
+		final Creature creature = new Creature();
+		creature.setDef(55);
+		creature.setArmor(-10);
+
+		assertEquals(0, creature.getArmor());
+	}
+}
