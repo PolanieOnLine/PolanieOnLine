325a62f8d13b6fa7a206169c43322d661cf306a3
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-08 03:13:34 +0200

    test(combat): cover per-weapon armor multiplier resolution

--- tests/games/stendhal/server/core/rule/damage/WeaponArmorInteractionServiceTest.java
+++ tests/games/stendhal/server/core/rule/damage/WeaponArmorInteractionServiceTest.java
@@ -6,8 +6,6 @@ package games.stendhal.server.core.rule.damage;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,7 +16,6 @@ import games.stendhal.server.core.rule.damage.WeaponArmorInteractionService.Armo
 import games.stendhal.server.entity.RPEntity;
 import games.stendhal.server.entity.creature.Creature;
 import games.stendhal.server.entity.item.Corpse;
-import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.item.Weapon;
 import utilities.RPClass.CreatureTestHelper;
 import utilities.RPClass.ItemTestHelper;
@@ -84,72 +81,40 @@ public class WeaponArmorInteractionServiceTest {
 	}
 
 	@Test
-	public void highDefenseWithoutArmorTypeRemainsUnarmored() {
+	public void highDefenseWithoutArmorTypeRemainsNeutral() {
 		final Creature defender = new Creature();
 		defender.setDef(150);
-		final Weapon weapon = weapon("dagger", 30);
 
-		assertEquals(50.0, WeaponArmorInteractionService.adjustAttack(
-				50.0, 50.0, Arrays.asList(weapon), weapon, defender),
-				0.000001);
+		assertEquals(1.0, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("dagger"), defender), 0.0);
 	}
 
 	@Test
-	public void explicitArmorTypeControlsMatchupIndependentlyOfDefense() {
+	public void explicitArmorTypeControlsMultiplierIndependentlyOfDefense() {
 		final Creature defender = new Creature();
 		defender.setDef(1);
 		defender.setArmorType("heavy");
-		final Weapon weapon = weapon("axe", 30);
 
-		assertEquals(expectedAdjusted(50.0, 50.0, weapon, 1.15),
-				WeaponArmorInteractionService.adjustAttack(50.0, 50.0,
-						Arrays.asList(weapon), weapon, defender), 0.000001);
+		assertEquals(1.15, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("axe"), defender), 0.0);
+		assertEquals(0.80, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("dagger"), defender), 0.0);
 	}
 
 	@Test
-	public void matchupUsesActualRolledWeaponContribution() {
-		final Creature defender = new Creature();
-		defender.setArmorType("light");
-		final Weapon weapon = weapon("dagger", 30);
-		final double stableAttack = 50.0;
-		final double rolledAttack = 44.0;
-
-		final double stableWeapon = weapon.getAverageDamage();
-		final double rolledWeapon = rolledAttack
-				- (stableAttack - stableWeapon);
-		final double expected = rolledAttack + rolledWeapon * 0.10;
-
-		assertEquals(expected, WeaponArmorInteractionService.adjustAttack(
-				rolledAttack, stableAttack, Arrays.asList(weapon), weapon,
-				defender), 0.000001);
-		assertTrue(Math.abs(expected - (rolledAttack + stableWeapon * 0.10))
-				> 0.000001);
-	}
-
-	@Test
-	public void pairedWeaponsAreAdjustedWithoutMultiplyingFlatEquipmentAttack() {
+	public void differentWeaponsResolveDifferentMultipliersAgainstSameTarget() {
 		final Creature defender = new Creature();
 		defender.setArmorType("heavy");
-		final Weapon first = weapon("axe", 20);
-		final Weapon second = weapon("axe", 10);
-		final double stableAttack = 80.0;
-		final double rolledAttack = 75.0;
-		final double stableWeapons = first.getAverageDamage()
-				+ second.getAverageDamage();
-		final double stableNonWeaponAttack = stableAttack - stableWeapons;
-		final double rolledWeapons = rolledAttack - stableNonWeaponAttack;
-		final double expected = rolledAttack + rolledWeapons * 0.15;
 
-		assertEquals(expected, WeaponArmorInteractionService.adjustAttack(
-				rolledAttack, stableAttack, Arrays.asList(first, second), first,
-				defender), 0.000001);
-		assertTrue(Math.abs(expected - rolledAttack * 1.15) > 0.000001);
+		assertEquals(0.80, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("dagger"), defender), 0.0);
+		assertEquals(1.15, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("axe"), defender), 0.0);
 	}
 
 	@Test
 	public void nonCreatureTargetRemainsNeutral() {
-		final Weapon weapon = weapon("dagger", 30);
-		final RPEntity playerTarget = new RPEntity() {
+		final RPEntity target = new RPEntity() {
 			@Override
 			protected void dropItemsOn(final Corpse corpse) {
 				// no items
@@ -161,31 +126,16 @@ public class WeaponArmorInteractionServiceTest {
 			}
 		};
 
-		assertEquals(50.0, WeaponArmorInteractionService.adjustAttack(
-				50.0, 50.0, Arrays.asList(weapon), weapon, playerTarget),
-				0.000001);
+		assertEquals(1.0, WeaponArmorInteractionService.getDamageMultiplier(
+				weapon("dagger"), target), 0.0);
 	}
 
 	@Test
 	public void missingWeaponRemainsNeutral() {
-		assertEquals(25.0, WeaponArmorInteractionService.adjustAttack(
-				25.0, 25.0, Collections.<Item>emptyList(), null, null),
-				0.000001);
-	}
-
-	@Test
-	public void negativeWeaponContributionCannotCorruptAttack() {
-		assertEquals(25.0,
-				WeaponArmorInteractionService.adjustWeaponContribution(
-						25.0, -10.0, 1.15), 0.000001);
-	}
-
-	private double expectedAdjusted(final double rolledAttack,
-			final double stableAttack, final Weapon weapon,
-			final double multiplier) {
-		final double rolledWeapon = rolledAttack
-				- (stableAttack - weapon.getAverageDamage());
-		return rolledAttack + rolledWeapon * (multiplier - 1.0);
+		final Creature defender = new Creature();
+		defender.setArmorType("heavy");
+		assertEquals(1.0, WeaponArmorInteractionService.getDamageMultiplier(
+				null, defender), 0.0);
 	}
 
 	private double multiplier(final String weaponClass,
@@ -194,9 +144,9 @@ public class WeaponArmorInteractionServiceTest {
 				weaponClass, armorType);
 	}
 
-	private Weapon weapon(final String itemClass, final int attack) {
+	private Weapon weapon(final String itemClass) {
 		final Map<String, String> attributes = new HashMap<String, String>();
-		attributes.put("atk", Integer.toString(attack));
+		attributes.put("atk", "30");
 		attributes.put("rate", "5");
 		return new Weapon("test weapon", itemClass, "test", attributes);
 	}
