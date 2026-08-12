fff0d7dcc352a6a812cbd059b79f0d36b63aef29
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-12 09:01:40 +0200

    Rozszerz test odświeżania flag sezonowych

--- tests/games/stendhal/common/constants/OccasionRuntimeTest.java
+++ tests/games/stendhal/common/constants/OccasionRuntimeTest.java
@@ -6,41 +6,106 @@ package games.stendhal.common.constants;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class OccasionRuntimeTest {
 	private static final String CHRISTMAS = "stendhal.christmas";
-	private String previous;
+	private static final String EASTER = "stendhal.easter";
+	private static final String MINETOWN = "stendhal.minetown";
+	private static final String MINETOWN_CONSTRUCTION = "stendhal.minetownconstruction";
+
+	private final Map<String, String> previous = new HashMap<String, String>();
 
 	@Before
-	public void rememberProperty() {
-		previous = System.getProperty(CHRISTMAS);
+	public void rememberProperties() {
+		remember(CHRISTMAS);
+		remember(EASTER);
+		remember(MINETOWN);
+		remember(MINETOWN_CONSTRUCTION);
 	}
 
 	@After
-	public void restoreProperty() {
-		if (previous == null) {
-			System.clearProperty(CHRISTMAS);
-		} else {
-			System.setProperty(CHRISTMAS, previous);
-		}
+	public void restoreProperties() {
+		restore(CHRISTMAS);
+		restore(EASTER);
+		restore(MINETOWN);
+		restore(MINETOWN_CONSTRUCTION);
 		Occasion.refresh();
 	}
 
 	@Test
 	public void refreshTracksRuntimeChristmasPropertyChanges() {
-		System.clearProperty(CHRISTMAS);
-		Occasion.refresh();
-		assertFalse(Occasion.CHRISTMAS.booleanValue());
+		assertRefreshTracks(CHRISTMAS, new OccasionValue() {
+			@Override
+			public boolean get() {
+				return Occasion.CHRISTMAS.booleanValue();
+			}
+		});
+	}
 
-		System.setProperty(CHRISTMAS, "true");
-		Occasion.refresh();
-		assertTrue(Occasion.CHRISTMAS.booleanValue());
+	@Test
+	public void refreshTracksRuntimeEasterPropertyChanges() {
+		assertRefreshTracks(EASTER, new OccasionValue() {
+			@Override
+			public boolean get() {
+				return Occasion.EASTER.booleanValue();
+			}
+		});
+	}
 
-		System.clearProperty(CHRISTMAS);
+	@Test
+	public void refreshTracksRuntimeMineTownPropertyChanges() {
+		assertRefreshTracks(MINETOWN, new OccasionValue() {
+			@Override
+			public boolean get() {
+				return Occasion.MINETOWN.booleanValue();
+			}
+		});
+	}
+
+	@Test
+	public void refreshTracksRuntimeMineTownConstructionPropertyChanges() {
+		assertRefreshTracks(MINETOWN_CONSTRUCTION, new OccasionValue() {
+			@Override
+			public boolean get() {
+				return Occasion.MINETOWN_CONSTRUCTION.booleanValue();
+			}
+		});
+	}
+
+	private void assertRefreshTracks(final String property, final OccasionValue value) {
+		System.clearProperty(property);
 		Occasion.refresh();
-		assertFalse(Occasion.CHRISTMAS.booleanValue());
+		assertFalse(value.get());
+
+		System.setProperty(property, "true");
+		Occasion.refresh();
+		assertTrue(value.get());
+
+		System.clearProperty(property);
+		Occasion.refresh();
+		assertFalse(value.get());
+	}
+
+	private void remember(final String property) {
+		previous.put(property, System.getProperty(property));
+	}
+
+	private void restore(final String property) {
+		final String value = previous.get(property);
+		if (value == null) {
+			System.clearProperty(property);
+		} else {
+			System.setProperty(property, value);
+		}
+	}
+
+	private interface OccasionValue {
+		boolean get();
 	}
 }
