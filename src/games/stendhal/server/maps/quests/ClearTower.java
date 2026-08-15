a299564796608ca81d77eb1401db4ee77a9ae1fc
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-16 01:09:47 +0200

    Przenieś Czyszczenie Wieży na nowy builder

--- src/games/stendhal/server/maps/quests/ClearTower.java
+++ src/games/stendhal/server/maps/quests/ClearTower.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2003-2021 - Stendhal                    *
+ *                   (C) Copyright 2003-2024 - PolanieOnLine               *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -11,160 +11,60 @@
  ***************************************************************************/
 package games.stendhal.server.maps.quests;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
-import games.stendhal.server.entity.npc.ChatAction;
-import games.stendhal.server.entity.npc.ConversationPhrases;
-import games.stendhal.server.entity.npc.ConversationStates;
-import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.action.IncreaseKarmaAction;
 import games.stendhal.server.entity.npc.action.IncreaseXPAction;
-import games.stendhal.server.entity.npc.action.MultipleActions;
-import games.stendhal.server.entity.npc.action.SetQuestAction;
-import games.stendhal.server.entity.npc.action.SetQuestAndModifyKarmaAction;
-import games.stendhal.server.entity.npc.action.StartRecordingKillsAction;
-import games.stendhal.server.entity.npc.condition.AndCondition;
-import games.stendhal.server.entity.npc.condition.GreetingMatchesNameCondition;
-import games.stendhal.server.entity.npc.condition.KilledForQuestCondition;
-import games.stendhal.server.entity.npc.condition.NotCondition;
-import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
-import games.stendhal.server.entity.npc.condition.QuestNotStartedCondition;
-import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.npc.quest.KillCreaturesQuestBuilder;
+import games.stendhal.server.entity.npc.quest.QuestManuscript;
 import games.stendhal.server.maps.Region;
-import marauroa.common.Pair;
 
-public class ClearTower extends AbstractQuest {
-	private static final String QUEST_SLOT = "clear_tower";
-	private final SpeakerNPC npc = npcs.get("Czarnoksiężnik");
-
-	private void step_1() {
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES, 
-				new QuestNotStartedCondition(QUEST_SLOT),
-				ConversationStates.QUEST_OFFERED,
-				"Jako pierwsze twe zadanie ode mnie, masz wyczyścić wieżę z zalęgłych tutaj potworów. Zrobisz to?",
-				null);
-
-		final List<ChatAction> start = new LinkedList<ChatAction>();
-		final HashMap<String, Pair<Integer, Integer>> toKill = 
-			new HashMap<String, Pair<Integer, Integer>>();
-			// first number is required solo kills, second is required shared kills
-			toKill.put("starszy gargulec", new Pair<Integer, Integer>(0,1));
-			toKill.put("mroczny gargulec", new Pair<Integer, Integer>(0,1));
-			toKill.put("trujący gargulec", new Pair<Integer, Integer>(0,1));
-			toKill.put("gargulec", new Pair<Integer, Integer>(0,1));
-			toKill.put("nietoperz", new Pair<Integer, Integer>(0,1));
-			toKill.put("nietoperz wampir", new Pair<Integer, Integer>(0,1));
-			toKill.put("pająk ptasznik", new Pair<Integer, Integer>(0,1));
-			toKill.put("pająk", new Pair<Integer, Integer>(0,1));
-			toKill.put("wściekły szczur", new Pair<Integer, Integer>(0,1));
-			toKill.put("krwiożerczy szczur", new Pair<Integer, Integer>(0,1));
-			toKill.put("szczur zombie", new Pair<Integer, Integer>(0,1));
-
-			start.add(new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 5.0));
-			start.add(new StartRecordingKillsAction(QUEST_SLOT, 1, toKill));
-
-		npc.add(
-				ConversationStates.QUEST_OFFERED,
-				ConversationPhrases.YES_MESSAGES,
-				null,
-				ConversationStates.ATTENDING,
-				"Dobrze. Wróć jak skończysz.",
-				new MultipleActions(start));
-
-		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.NO_MESSAGES, null,
-				ConversationStates.ATTENDING,
-				"No cóż... już na początku wiedziałem, że się nie nadasz.",
-				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0));
-	}
-
-	private void step_2() {
-		/* Player has to kill the monsters in tower */
-	}
-
-	private void step_3() {
-		final List<ChatAction> reward = new LinkedList<ChatAction>();
-			reward.add(new IncreaseKarmaAction(15.0));
-			reward.add(new IncreaseXPAction(10000));
-			reward.add(new SetQuestAction(QUEST_SLOT, "done"));
-
-		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
-				new AndCondition(new GreetingMatchesNameCondition(npc.getName()),
-						new QuestInStateCondition(QUEST_SLOT, 0, "start"), new KilledForQuestCondition(QUEST_SLOT,1)),
-				ConversationStates.IDLE, "Dobrze, więc będziesz miał drugie #'zadanie' ode mnie...",
-				new MultipleActions(reward));
-
-		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
-				new AndCondition(new GreetingMatchesNameCondition(npc.getName()),
-						new QuestInStateCondition(QUEST_SLOT, 0, "start"), new NotCondition(new KilledForQuestCondition(QUEST_SLOT, 1))),
-				ConversationStates.IDLE,
-				"Jeszcze nie skończyłeś, wróć do mnie jak wybijesz ~'wszystkie' potwory w wieży.",
-				null);
-	}
-
+public class ClearTower implements QuestManuscript {
 	@Override
-	public void addToWorld() {
-		fillQuestInfo(
-				"Wyczyszczenie Starej Wieży",
-				"Czarnoksiężnik kazał wyczyścić wieżę, aby sprawdzić jakie podstawowe umiejętności posiadasz.",
-				false);
-		step_1();
-		step_2();
-		step_3();
-	}
+	public KillCreaturesQuestBuilder story() {
+		KillCreaturesQuestBuilder quest = new KillCreaturesQuestBuilder();
 
-	@Override
-	public List<String> getHistory(final Player player) {
-		final List<String> res = new ArrayList<String>();
-		if (!player.hasQuest(QUEST_SLOT)) {
-			return res;
-		}
-		res.add(player.getGenderVerb("Spotkałem") + " Czarnoksiężnika w jakiejś starej wieży.");
-		final String questState = player.getQuest(QUEST_SLOT, 0);
-		if ("rejected".equals(questState)) {
-			res.add(player.getGenderVerb("Odmówiłem") + " Czarnoksiężnikowi pomocy.");
-		return res;
-		}
-		res.add(player.getGenderVerb("Postanowiłem") + " pomóc Czarnoksiężnikowi w pozbyciu się zalęgłych potworów.");
-		if (("start".equals(questState) && player.hasKilled("starszy gargulec") && player.hasKilled("mroczny gargulec")
-				&& player.hasKilled("trujący gargulec") && player.hasKilled("gargulec")
-				&& player.hasKilled("nietoperz wampir") && player.hasKilled("nietoperz")
-				&& player.hasKilled("pająk") && player.hasKilled("pająk ptasznik")
-				&& player.hasKilled("wściekły szczur") && player.hasKilled("krwiożerczy szczur")
-				&& player.hasKilled("szczur zombie")) || "done".equals(questState)) {
-			res.add("Wieża została wyczyszczona.");
-		}
-		if ("done".equals(questState)) {
-			res.add("Czarnoksiężnik będzie miał dla mnie drugie zadanie.");
-		}
-		return res;
-	}
+		quest.info()
+			.name("Wyczyszczenie Starej Wieży")
+			.description("Czarnoksiężnik chce odzyskać starą wieżę i sprawdzić, czy potrafisz poradzić sobie z jej mieszkańcami.")
+			.internalName("clear_tower")
+			.notRepeatable()
+			.minLevel(35)
+			.region(Region.ZAKOPANE_CITY)
+			.questGiverNpc("Czarnoksiężnik");
 
-	@Override
-	public int getMinLevel() {
-		return 35;
-	}
+		quest.history()
+			.whenNpcWasMet("Spotkałem Czarnoksiężnika w starej wieży niedaleko Zakopanego.")
+			.whenQuestWasRejected("Odmówiłem oczyszczenia starej wieży.")
+			.whenQuestWasAccepted("Zgodziłem się oczyścić starą wieżę z potworów, które zajęły jej wnętrze.")
+			.whenTaskWasCompleted("Wieża została oczyszczona. Mogę wrócić do Czarnoksiężnika.")
+			.whenQuestWasCompleted("Czarnoksiężnik uznał, że poradziłem sobie z pierwszą próbą i zapowiedział kolejne zadanie.");
 
-	@Override
-	public String getName() {
-		return "Wyczyszczenie Starej Wieży";
-	}
+		quest.offer()
+			.respondToRequest("Zanim powierzę ci poważniejsze sprawy, chcę zobaczyć jak radzisz sobie w walce. W mojej starej wieży zalęgły się potwory i przeszkadzają mi w pracy. Oczyścisz ją?")
+			.respondToUnrepeatableRequest("Wieża jest już oczyszczona. Teraz mogę zająć się tym, co naprawdę mnie interesuje.")
+			.respondToAccept("Dobrze. Oczyść wieżę dokładnie i wróć do mnie, gdy żaden z jej mieszkańców nie będzie już stanowił problemu.")
+			.respondToReject("Szkoda. W takim razie nie mam powodu powierzać ci trudniejszych spraw.")
+			.rejectionKarmaPenalty(10.0)
+			.acceptWith(new IncreaseKarmaAction(5.0))
+			.remind("W starej wieży nadal są potwory. Oczyść ją dokładnie, zanim do mnie wrócisz.");
 
-	@Override
-	public String getNPCName() {
-		return npc.getName();
-	}
+		quest.task()
+			.requestKill(1, "starszy gargulec")
+			.requestKill(1, "mroczny gargulec")
+			.requestKill(1, "trujący gargulec")
+			.requestKill(1, "gargulec")
+			.requestKill(1, "nietoperz")
+			.requestKill(1, "nietoperz wampir")
+			.requestKill(1, "pająk ptasznik")
+			.requestKill(1, "pająk")
+			.requestKill(1, "wściekły szczur")
+			.requestKill(1, "krwiożerczy szczur")
+			.requestKill(1, "szczur zombie");
 
-	@Override
-	public String getSlotName() {
-		return QUEST_SLOT;
-	}
+		quest.complete()
+			.greet("Dobrze. Wieża znowu nadaje się do pracy. Skoro poradziłeś sobie z pierwszą próbą, mam dla ciebie kolejne zadanie.")
+			.rewardWith(new IncreaseXPAction(10000))
+			.rewardWith(new IncreaseKarmaAction(15.0));
 
-	@Override
-	public String getRegion() {
-		return Region.ZAKOPANE_CITY;
+		return quest;
 	}
 }
