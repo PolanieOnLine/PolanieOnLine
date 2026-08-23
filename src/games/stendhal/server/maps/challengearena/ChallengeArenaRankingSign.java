65f83e6df849e9872b0411a8a791c8a47702f6e5
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-23 21:05:09 +0200

    feat(arena): add challenge arena hall of fame sign

---
+++ src/games/stendhal/server/maps/challengearena/ChallengeArenaRankingSign.java
@@ -0,0 +1,26 @@
+/***************************************************************************
+ *                   (C) Copyright 2026 - PolanieOnLine                   *
+ ***************************************************************************/
+package games.stendhal.server.maps.challengearena;
+
+import games.stendhal.server.core.events.TurnNotifier;
+import games.stendhal.server.entity.mapstuff.sign.Sign;
+import games.stendhal.server.entity.mapstuff.sign.SignFromHallOfFameLoader;
+
+/** Displays the ten best Challenge Arena competitors. */
+public final class ChallengeArenaRankingSign extends Sign {
+	private static final int SIGN_LENGTH = 10;
+
+	public ChallengeArenaRankingSign() {
+		put("class", "book_blue");
+		setText("Najlepsi wojownicy Areny Wyzwań");
+		updatePlayers();
+	}
+
+	public void updatePlayers() {
+		final SignFromHallOfFameLoader loader = new SignFromHallOfFameLoader(
+				this, "Najlepsi wojownicy Areny Wyzwań\n",
+				ChallengeArenaRankingService.FAME_TYPE, SIGN_LENGTH, false, true);
+		TurnNotifier.get().notifyInTurns(0, loader);
+	}
+}
