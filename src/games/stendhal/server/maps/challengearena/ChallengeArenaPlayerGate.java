8907e86b546bab93eb8e1d7c096875ad7856c022
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-08-23 21:13:19 +0200

    fix(challenge-arena): restrict combat ring to paid challenger

---
+++ src/games/stendhal/server/maps/challengearena/ChallengeArenaPlayerGate.java
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *                   (C) Copyright 2026 - PolanieOnLine                   *
+ ***************************************************************************/
+package games.stendhal.server.maps.challengearena;
+
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.mapstuff.area.OnePlayerArea;
+import games.stendhal.server.entity.player.Player;
+
+/** Blocks the combat ring unless the player owns the paid arena reservation. */
+public final class ChallengeArenaPlayerGate extends OnePlayerArea {
+	public ChallengeArenaPlayerGate(final int width, final int height) {
+		super(width, height);
+	}
+
+	@Override
+	public boolean isObstacle(final Entity entity) {
+		if (!(entity instanceof Player)) {
+			return super.isObstacle(entity);
+		}
+		final Player player = (Player) entity;
+		if (player.isGhost() || contains(player)) {
+			return false;
+		}
+		if (!ChallengeArenaManager.isReservedBy(player.getName())) {
+			return true;
+		}
+		return super.isObstacle(entity);
+	}
+}
