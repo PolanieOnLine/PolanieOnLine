a2633ed110b807335f3ffad2ba4c6114d802ecb9
PolanieOnLine <50992387+PolanieOnLine@users.noreply.github.com>
2026-09-07 19:07:36 +0200

    Fix webclient column layout after login

--- src/js/stendhal/PerceptionListener.ts
+++ src/js/stendhal/PerceptionListener.ts
@@ -60,7 +60,7 @@ export class StendhalPerceptionListener extends PerceptionListener {
 			window.setTimeout(function() {
 				let body = document.getElementById("body")!;
 				body.style.cursor = "auto";
-				document.getElementById("client")!.style.display = "block";
+				document.getElementById("client")!.style.display = "flex";
 				document.getElementById("loginpopup")!.style.display = "none";
 
 				// initialize observer after UI is ready
